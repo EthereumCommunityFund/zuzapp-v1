003ffc926f16aa6a5ee2328c53c9be047908e693
davik stone <50995003+davik20@users.noreply.github.com>
2023-08-30 16:36:55 +0100

    update tsconfig

---
+++ apps/events-app/pages/api/events/index.ts
@@ -0,0 +1,10 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+
+function handler(req: NextApiRequest, res: NextApiResponse) {
+
+
+    console.log("events index")
+}
+
+export default handler
