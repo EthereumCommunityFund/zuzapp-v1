da79364d47ea1a62cadbc8fda5c0b34e473feef5
davik stone <50995003+davik20@users.noreply.github.com>
2023-08-30 16:36:55 +0100

    update tsconfig

--- apps/events-app/pages/api/events/[id].ts
+++ apps/events-app/pages/api/events/[id].ts
@@ -4,7 +4,7 @@ import withSession from "../../../middleware/withSession"
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 
-    console.log('testing')
+    console.log(req, res)
 }
 
 
