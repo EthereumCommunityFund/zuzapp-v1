6c68878262e92613e818be99c4437237bb4f14e0
MightyDevKnight <james@ecf.network>
2023-09-07 22:32:51 +0200

    update dashboard

--- apps/events-app/components/ui-providers/DashboardLayout.tsx
+++ apps/events-app/components/ui-providers/DashboardLayout.tsx
@@ -17,17 +17,19 @@ export const DashboardProvider = ({ children }: { children: React.ReactNode }) =
     if (!router.pathname.startsWith("/dashboard")) return <div className="bg-[#222222] text-white relative min-h-screen">{children}</div>
     return (
         <>
-        <div className="flex relative bg-[#222222] text-white">
+        <div className="flex relative bg-[#222222] text-white h-[100vh]">
           <DashboardNavigation />
             <DashboardHeader />
-          <div className="mt-20 w-full overflow-y-auto h-[90vh]">
-            {
-              !checkIfCurrentRouteIsInDashboardRoutes() && (
-                  <SubHeader />
-                  )
-                }
-            <div className="flex-1 mx-auto mt-10 px-10">
-              {children}
+          <div className="mt-20 w-full">
+            <div className="h-[90vh] overflow-y-auto">
+              {
+                !checkIfCurrentRouteIsInDashboardRoutes() && (
+                    <SubHeader />
+                    )
+                  }
+              <div className="flex-1 mx-auto mt-10 px-10">
+                {children}
+              </div>   
             </div>
           </div>
         </div>
