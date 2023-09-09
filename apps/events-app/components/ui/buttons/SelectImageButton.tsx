a8854f55fc3a4b20ed70cc425908dfd2679b0ead
MightyDevKnight <james@ecf.network>
2023-09-09 22:48:55 +0200

    add track page

--- apps/events-app/components/ui/buttons/SelectImageButton.tsx
+++ apps/events-app/components/ui/buttons/SelectImageButton.tsx
@@ -8,7 +8,7 @@ interface IProps {
 
 export default function SelectImageButton (props: IProps) {
   return (
-    <Button className="flex flex-col py-2.5 px-3 gap-[10px] self-stretch rounded-[6px] border-dashed border-white border-opacity-10 bg-[#242727] w-full">
+    <Button variant="light-dark" className="flex flex-col py-2.5 px-3 gap-[10px] self-stretch rounded-[6px] border-dashed border-white border-opacity-10 bg-[#242727] w-full">
       <HiUpload />
       <span className="text-[13px] text-center opacity-50 font-bold leading-[1.2] self-stretch">{props.title}</span>
       <span className=" text-[10px] tracking-[0.2px] font-normal leading-[1.2] opacity-50">DRAG & DROP IMAGE</span>
---
+++ apps/events-app/components/ui/labels/image-upload-button-description.tsx
@@ -0,0 +1,9 @@
+interface IImageUploadButtonDescription {
+  name: string
+}
+export default function ImageUploadButtonDescription(props: IImageUploadButtonDescription) {
+  const { name } = props;
+  return (
+    <div className="opacity-70 h-3 font-normal text-[10px] leading-3">{name}</div>
+  )
+}
