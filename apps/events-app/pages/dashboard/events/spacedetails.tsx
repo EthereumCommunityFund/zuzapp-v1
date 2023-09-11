f99b37b2e1ac9b35df1df2ddb673b04a85274c2a
MightyDevKnight <james@ecf.network>
2023-09-11 23:02:10 +0200

    fix eventspacedetails name

--- apps/events-app/pages/dashboard/events/spacedetails.tsx
+++ apps/events-app/pages/dashboard/events/spacedetails.tsx
@@ -1,12 +1,12 @@
 import EventLocation from "@/components/eventspace/EventLocation";
-import EventSpaceDeatils from "@/components/eventspace/EventSpaceDetails";
+import EventSpaceDetails from "@/components/eventspace/EventSpaceDetails";
 import EventSpaceDetailsNavBar from "@/components/eventspace/EventSpaceDetailsNavBar";
 import Button from "@/components/ui/buttons/Button";
 import Link from "next/link";
 import { HiArrowLeft } from "react-icons/hi";
 
 
-export default function EventSpaceDeatilsPage() {
+export default function EventSpaceDetailsPage() {
   return (
     <div className="flex py-5 px-10 flex-col flex-1 items-center gap-[10px] self-stretch">
       <div className="flex items-start gap-8 self-stretch">
@@ -15,7 +15,7 @@ export default function EventSpaceDeatilsPage() {
           <Link href={"spacedashboard"}>
             <Button leftIcon={HiArrowLeft} variant="light-dark">Back</Button>
           </Link>
-          <EventSpaceDeatils />
+          <EventSpaceDetails />
           <EventLocation />
         </div>
       </div>
