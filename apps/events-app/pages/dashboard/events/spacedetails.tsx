e8ed5c1bc943ef705e815cdf98fb6c464cb6bb92
dev_waliba <donwaleyb@gmail.com>
2023-09-12 23:50:04 +0100

    minor changes

--- apps/events-app/pages/dashboard/events/spacedetails.tsx
+++ apps/events-app/pages/dashboard/events/spacedetails.tsx
@@ -1,10 +1,10 @@
-import EventLocation from "@/components/eventspace/EventLocation";
-import EventSpaceDetails from "@/components/eventspace/EventSpaceDetails";
-import EventSpaceDetailsNavBar from "@/components/eventspace/EventSpaceDetailsNavBar";
-import Button from "@/components/ui/buttons/Button";
-import Link from "next/link";
-import { HiArrowLeft } from "react-icons/hi";
-
+import EventLocation from '@/components/eventspace/EventLocation';
+import EventSpaceDetails from '@/components/eventspace/EventSpaceDetails';
+import EventSpaceDetailsNavBar from '@/components/eventspace/EventSpaceDetailsNavBar';
+import Button from '@/components/ui/buttons/Button';
+import { createPagesServerClient } from '@supabase/auth-helpers-nextjs';
+import Link from 'next/link';
+import { HiArrowLeft } from 'react-icons/hi';
 
 export default function EventSpaceDetailsPage() {
   return (
@@ -12,13 +12,41 @@ export default function EventSpaceDetailsPage() {
       <div className="flex items-start gap-8 self-stretch">
         <EventSpaceDetailsNavBar />
         <div className="flex flex-col px-5 gap-5 justify-center items-start flex-1">
-          <Link href={"spacedashboard"}>
-            <Button leftIcon={HiArrowLeft} variant="light-dark">Back</Button>
+          <Link href={'spacedashboard'}>
+            <Button leftIcon={HiArrowLeft} variant="light-dark">
+              Back
+            </Button>
           </Link>
           <EventSpaceDetails />
           <EventLocation />
         </div>
       </div>
     </div>
-  )
+  );
 }
+
+export const getServerSideProps = async (ctx: any) => {
+  const supabase = createPagesServerClient(ctx);
+  let {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  if (!session)
+    return {
+      props: {
+        initialSession: null,
+        user: null,
+      },
+    };
+
+  // get profile from session
+  const { data: profile, error } = await supabase.from('profile').select('*').eq('uuid', session.user.id);
+
+  return {
+    props: {
+      initialSession: session,
+      user: session?.user,
+      profile: profile,
+    },
+  };
+};
