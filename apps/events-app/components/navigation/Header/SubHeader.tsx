3d257c3e970279ded963ca598862051b979d17e9
MightyDevKnight <james@ecf.network>
2023-09-12 06:21:01 +0200

    Create tracks/schedules dashboard page

--- apps/events-app/components/navigation/Header/SubHeader.tsx
+++ apps/events-app/components/navigation/Header/SubHeader.tsx
@@ -1,18 +1,22 @@
+import { useState } from "react";
 import { useRouter } from "next/router";
 import { BsArrowLeft } from "react-icons/bs";
-import { tabButtonLists } from "./TabButtons"; 
+import { tabButtonLists } from "./TabButtons";
 import TabButton from "./TabButton";
 import Event from "./Event";
-import { Popover } from "@/components/ui/popover";
+
 import AddToEventButton from "./AddToEvent";
 
 export default function SubHeader() {
   const router = useRouter();
+  const [activeTab, setActiveTab] = useState(null);
+
   const goBackToPreviousPage = () => {
     router.back();
   };
-  const goTabButton = (path: string) => {
+  const goTabButton = (path: string, index: any) => {
     router.push(path);
+    setActiveTab(index);
   }
   return (
     <>
@@ -21,13 +25,19 @@ export default function SubHeader() {
           <button className="flex items-center pr-2 pl-2 font-semibold" onClick={goBackToPreviousPage}>
             <BsArrowLeft className="mr-1" /> Exit
           </button>
-          <Event name={"ZuConnect"}/>
+          <Event name={"ZuConnect"} />
         </div>
         <div className="flex">
           {
-            tabButtonLists.map((tabButton) => {
+            tabButtonLists.map((tabButton, index) => {
               return (
-                <TabButton name={tabButton.name} ButtonIcon={tabButton.icon} onClick={() => goTabButton(tabButton.path)}/>
+                <TabButton
+                  key={index}
+                  name={tabButton.name}
+                  ButtonIcon={tabButton.icon}
+                  onClick={() => goTabButton(tabButton.path, index)}
+                  isActive={index === activeTab}
+                />
               )
             })
           }
