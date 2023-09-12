d3581c8f3c73f79ae7f3b3a43502c0f0400e5546
MightyDevKnight <james@ecf.network>
2023-09-12 15:09:23 +0200

    resolve issues

--- apps/events-app/components/templates/ImageUploadForm.tsx
+++ apps/events-app/components/templates/ImageUploadForm.tsx
@@ -1,23 +1,17 @@
 import ImageUploadButton from "../ui/buttons/SelectImageButton";
-<<<<<<< HEAD
 import ImageUploadButtonDescription from "../ui/labels/image-upload-button-description";
-=======
->>>>>>> df0c4f1 (feat: add tracks page)
 import InputFieldLabel from "../ui/labels/input-field-label";
 
 interface IProps {
-  title: string;  
+  title: string;
 }
 
 export default function ImageUploadForm(props: IProps) {
   return (
     <div className="flex flex-col items-start gap-[10px] self-stretch">
       <InputFieldLabel name={`${props.title} Image`} />
-      <ImageUploadButton title="Select Track Image"/>
-<<<<<<< HEAD
-      <ImageUploadButtonDescription name="We recommend using at least a 420 X 320"/>
-=======
->>>>>>> df0c4f1 (feat: add tracks page)
+      <ImageUploadButton title="Select Track Image" />
+      <ImageUploadButtonDescription name="We recommend using at least a 420 X 320" />
     </div>
   )
 }
