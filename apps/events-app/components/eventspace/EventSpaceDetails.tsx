839a87e9ef269157f90a2279f552561e7bc86b38
MightyDevKnight <james@ecf.network>
2023-09-12 11:47:55 +0200

    resolve issue in rebase

--- apps/events-app/components/eventspace/EventSpaceDetails.tsx
+++ apps/events-app/components/eventspace/EventSpaceDetails.tsx
@@ -1,12 +1,13 @@
-import { HiArrowLeft } from "react-icons/hi";
-
-import Button from "../ui/buttons/Button";
+import { useState } from "react";
+import Container from "../ui/Container";
 import EventBasics from "./EventBasics";
 import EventCategoriesLabs from "./EventCategoriesLabs";
 
 import EventFormat from "./EventFormat";
 import EventLinks from "./EventLinks";
 import EditionButtons from "@/components/ui/buttons/EditionButtons";
+import { CgClose } from "react-icons/cg";
+import { FaCircleArrowDown } from "react-icons/fa6";
 
 
 export default function EventSpaceDetails() {
@@ -20,7 +21,7 @@ export default function EventSpaceDetails() {
         }} />
         <EventLinks />
         <EventCategoriesLabs />
-        <EditionButtons type="Event-Space-Details" />
+        <EditionButtons type={"eventspace"} leftButtonName={"Discard"} rightButtonName={"Save Edit"} leftButtonIcon={CgClose} rightButtonIcon={FaCircleArrowDown} />
       </div>
     </Container>
   )
--- apps/events-app/components/schedule/ScheduleLabels.tsx
+++ apps/events-app/components/schedule/ScheduleLabels.tsx
@@ -1,5 +1,24 @@
+import { GoXCircle } from "react-icons/go";
+import SectionInputForm from "../ui/SectionInputForm";
+import InputFieldLabel from "../ui/labels/input-field-label";
+
 export default function ScheduleLabels() {
   return (
-    <span>ScheduleLabels</span>
+    <>
+      <span className="text-lg opacity-70 self-stretch">Roles</span>
+      <SectionInputForm title={"Select Event Category"} defaultValue={"Workshop"} inputType={"dropdown"} />
+      <SectionInputForm title={"Select Experience Level"} defaultValue={"Beginner"} inputType={"dropdown"} />
+      <div className="flex flex-col items-start gap-6 self-stretch">
+        <SectionInputForm title={"Add Tags"} defaultValue={"Workshop"} inputType={"iconinput"} description="Add multiple tags separated by commas" />
+        <div className="flex items-start gap-[10px]">
+          <div className="flex gap-2.5 items-center rounded-[8px] px-2 py-1.5 bg-white bg-opacity-10">
+            <button className="flex gap-2.5 items-center"><GoXCircle className="top-0.5 left-0.5 w-4 h-4" /><InputFieldLabel name={"TagOne"} /></button>
+          </div>
+          <div className="flex gap-2.5 items-center rounded-[8px] px-2 py-1.5 bg-white bg-opacity-10">
+            <button className="flex gap-2.5 items-center"><GoXCircle className="top-0.5 left-0.5 w-4 h-4" /><InputFieldLabel name={"TagTwo"} /></button>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
