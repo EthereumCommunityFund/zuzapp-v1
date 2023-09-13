290899497bf86c7ba2287d2b5add31df15fe784a
MightyDevKnight <james@ecf.network>
2023-09-13 03:11:28 +0200

    Fix styles on tracks page

--- apps/events-app/pages/dashboard/events/tracks.tsx
+++ apps/events-app/pages/dashboard/events/tracks.tsx
@@ -6,17 +6,17 @@ import { HiCog, HiPlusCircle, HiSelector } from "react-icons/hi";
 
 export default function Tracks() {
   return (
-    <div className="flex flex-col flex-1 items-center gap-[10px] p-10 self-stretch">
-      <div className="flex flex-col items-center gap-[44px] self-stretch">
+    <div className="flex flex-col flex-1 items-center gap-[10px] pt-10 px-20 pb-0 self-stretch">
+      <div className="flex flex-col items-center gap-[44px] self-stretch mx-auto w-[82%]">
         <div className="flex flex-col items-start gap-6 self-stretch">
-          <span className="text-[39px] font-bold leading-[1.2]">Tracks</span>
+          <span className="text-[50px] font-bold leading-[1.2]">Tracks</span>
           <div className="flex justify-between items-start self-stretch">
             <Link href={"addtrack"}>
-              <Button variant="light-blue" className="flex py-[10px] px-[14px] items-center gap-[10px] rounded-[20px] bg-[#67DAFF20] text-[#67DAFF]" leftIcon={BiSolidPlusCircle}>Add a Track</Button>
+              <Button variant="light-blue" className="rounded-full font-bold" size="lg" leftIcon={BiSolidPlusCircle}>Add a Track</Button>
             </Link>
             <div className="flex items-start gap-3">
-              <Button variant="ghost" leftIcon={HiSelector}>Sort</Button>
-              <Button variant="ghost" leftIcon={HiCog}>Select</Button>
+              <Button variant="ghost" size="lg" leftIcon={HiSelector}>Sort</Button>
+              <Button variant="ghost" size="lg" leftIcon={HiCog}>Select</Button>
             </div>
           </div>
           {
