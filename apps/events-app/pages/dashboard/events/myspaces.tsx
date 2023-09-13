45847858dc4b4127e0101c063da6affcdcc05877
MightyDevKnight <james@ecf.network>
2023-09-14 00:13:38 +0200

    resolve issue in conflicts

--- apps/events-app/pages/dashboard/events/myspaces.tsx
+++ apps/events-app/pages/dashboard/events/myspaces.tsx
@@ -1,42 +1,55 @@
-import EventSpacesTemplate from "@/components/templates/events/EventSpacesTemplate"
-import { createPagesServerClient } from "@supabase/auth-helpers-nextjs"
+import EventSpacesTemplate from '@/components/templates/events/EventSpacesTemplate';
+import { createPagesServerClient } from '@supabase/auth-helpers-nextjs';
+import { useEffect, useState } from 'react';
+import { fetchUserEventSpaces } from './eventSpaceService';
+import { EventSpaceDetailsType } from '@/types';
 
 export default function MyEventSpacesPage() {
   // Make request to get all event spaces
+  const [eventSpaces, setEventSpaces] = useState<EventSpaceDetailsType[]>([]);
 
+  useEffect(() => {
+    const fetchEventSpaces = async () => {
+      try {
+        const eventSpaceDetails = await fetchUserEventSpaces();
+        setEventSpaces(eventSpaceDetails);
+        console.log(eventSpaces, 'eventSpaces');
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchEventSpaces();
+  }, []);
   return (
     <div className="flex gap-[10px] flex-1 items-center self-stretch">
-      <EventSpacesTemplate />
+      <EventSpacesTemplate eventSpaces={eventSpaces} />
     </div>
-  )
+  );
 }
 
 export const getServerSideProps = async (ctx: any) => {
-  const supabase = createPagesServerClient(ctx)
+  const supabase = createPagesServerClient(ctx);
   let {
     data: { session },
-  } = await supabase.auth.getSession()
-
+  } = await supabase.auth.getSession();
 
   if (!session)
     return {
       props: {
         initialSession: null,
-        user: null
+        user: null,
       },
-    }
+    };
 
-  // get profile from session 
-  const { data: profile, error } = await supabase
-    .from('profile')
-    .select('*')
-    .eq('uuid', session.user.id);
+  // get profile from session
+  const { data: profile, error } = await supabase.from('profile').select('*').eq('uuid', session.user.id);
 
   return {
     props: {
       initialSession: session,
       user: session?.user,
-      profile: profile
+      profile: profile,
     },
-  }
-}
+  };
+};
--- apps/events-app/types/index.ts
+++ apps/events-app/types/index.ts
@@ -1,81 +1,86 @@
-import { Database } from "@/database.types";
+import { Database } from '@/database.types';
 
 export type EventSpaceUpdateRequestBody = {
-    id: string;
-    name: string;
-    event_space_type: "tracks" | "schedules";
-    status: "draft" | "published" | "archived";
-    start_date: number;
-    end_date: number;
-    description: string;
-    format: "in-person" | "online" | "hybrid";
-    event_type?: string[];
-    experience_level?: string[];
-    eventspacelocation?: LocationType[];
-}
+  id: string;
+  name: string;
+  event_space_type: 'tracks' | 'schedules';
+  status: 'draft' | 'published' | 'archived';
+  start_date: number;
+  end_date: number;
+  description: string;
+  format: 'in-person' | 'online' | 'hybrid';
+  event_type?: string[];
+  experience_level?: string[];
+  eventspacelocation?: LocationType[];
+};
 export type EventSpaceCreateRequestBody = {
-    name: string;
-    event_space_type: "tracks" | "schedules";
-}
-
+  name: string;
+  event_space_type: 'tracks' | 'schedules';
+};
+export type EventSpaceDetailsType = {
+  id: string;
+  name: string;
+  event_space_type: 'tracks' | 'schedules';
+  status: 'draft' | 'published' | 'archived';
+  start_date: number;
+  end_date: number;
+  description: string;
+  format: 'in-person' | 'online' | 'hybrid';
+  event_type?: string[];
+  experience_level?: string[];
+  eventspacelocation?: LocationType[];
+};
 export type LocationType = {
-    id?: string,
-    name: string;
-    description: string;
-    is_main: boolean;
-    address: string;
-    capacity: number;
-    image_urls?: string[];
-}
-
-
+  id?: string;
+  name: string;
+  description: string;
+  is_main: boolean;
+  address: string;
+  capacity: number;
+  image_urls?: string[];
+};
 
 export type ScheduleCreateRequestBody = {
-    name: string;
-    format: 'in-person' | 'online' | 'hybrid';
-    description: string;
-    date: number;
-    start_time: number;
-    end_time: number;
-    all_day?: boolean;
-    schedule_frequency: 'once' | 'everyday' | 'weekly';
-    images?: string[];
-    video_call_link?: string;
-    live_stream_url?: string;
-    location_id: string;
-    event_type?: string[];
-    experience_level?: string[];
-    limit_rsvp?: boolean;
-    rsvp_amount?: number;
-    event_space_id: string;
-    track_id?: string;
-    tags: string[],
-    speakers: [{
-        speaker_name: string,
-        role: string
-    }]
-}
-
-
+  name: string;
+  format: 'in-person' | 'online' | 'hybrid';
+  description: string;
+  date: number;
+  start_time: number;
+  end_time: number;
+  all_day?: boolean;
+  schedule_frequency: 'once' | 'everyday' | 'weekly';
+  images?: string[];
+  video_call_link?: string;
+  live_stream_url?: string;
+  location_id: string;
+  event_type?: string[];
+  experience_level?: string[];
+  limit_rsvp?: boolean;
+  rsvp_amount?: number;
+  event_space_id: string;
+  track_id?: string;
+  tags: string[];
+  speakers: [
+    {
+      speaker_name: string;
+      role: string;
+    },
+  ];
+};
 
 export type TrackCreateRequestBody = {
-    description: string;
-    event_space_id: string;
-    image: string | null;
-    name: string;
-}
+  description: string;
+  event_space_id: string;
+  image: string | null;
+  name: string;
+};
 export type TrackUpdateRequestBody = {
-    id?: string;
-    description: string | null;
-    event_space_id: string;
-    image: string | null;
-    name: string;
-}
-
-
-
-
-
+  id?: string;
+  description: string | null;
+  event_space_id: string;
+  image: string | null;
+  name: string;
+};
 
 export type QueryWithID = {
     [key: string]: string
