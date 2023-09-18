126aa0ebbb8d8dc1298c0b7d0cd5a0e93066a3fe
davik20 <onyejivic@gmail.com>
2023-09-18 11:35:53 +0100

    update linting

--- apps/events-app/.eslintrc.js
+++ apps/events-app/.eslintrc.js
@@ -1,3 +1,45 @@
 module.exports = {
-  extends: ["custom/next"],
+  // extends: ["custom/next"],
+
+    "env": {
+      "browser": true,
+      "es6": true,
+      "node": true
+    },
+    "extends": [
+      "eslint:recommended",
+      "plugin:react/recommended",
+      "plugin:@typescript-eslint/recommended",
+      "plugin:react-hooks/recommended",
+      "plugin:import/errors",
+      "plugin:import/warnings",
+      "plugin:import/typescript"
+    ],
+    "settings": {
+      "react": {
+        "version": "detect"
+      }
+    },
+    "parser": "espree",
+    "parserOptions": {
+      "ecmaFeatures": {
+        "jsx": true
+      },
+      "ecmaVersion": 2018,
+      "sourceType": "module"
+    },
+    "plugins": [
+      "react",
+      "@typescript-eslint",
+      "react-hooks"
+    ],
+    "rules": {
+      "react/react-in-jsx-scope": "off",
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "import/no-unresolved": "error"
+    }
+  
 };
+
