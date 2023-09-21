5d969fd60b01e598eb7faff7772c4080a1666411
MightyDevKnight <james@ecf.network>
2023-09-20 22:01:22 -0700

    Add eventviewtrack page

---
+++ apps/events-app/components/ui/TrackScheduleSection.tsx
@@ -0,0 +1,12 @@
+export default function TrackScheduleSection() {
+  return (
+    <div className="flex flex-col gap-5">
+      <h2 className="p-2.5 font-bold">Monday, October 30</h2>
+      <div className="flex flex-col gap-2.5 ">
+        {
+
+        }
+      </div>
+    </div>
+  )
+}
