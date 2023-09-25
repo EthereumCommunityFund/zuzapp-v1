7281f3181a863f33febffb70adacf203183b7def
davik20 <onyejivic@gmail.com>
2023-09-25 13:12:05 +0100

    fix types

--- apps/events-app/pages/dashboard/events/space/tracks/schedules/index.tsx
+++ apps/events-app/pages/dashboard/events/space/tracks/schedules/index.tsx
@@ -1,17 +1,26 @@
-import { BiLeftArrowAlt, BiEditAlt, BiPlus, BiRadioCircle, BiCalendarAlt, BiTimeFive, BiRadioCircleMarked, BiPlusCircle } from 'react-icons/bi';
-import { HiArrowLeft, HiCog, HiSelector } from 'react-icons/hi';
+import {
+  BiLeftArrowAlt,
+  BiEditAlt,
+  BiPlus,
+  BiRadioCircle,
+  BiCalendarAlt,
+  BiTimeFive,
+  BiRadioCircleMarked,
+  BiPlusCircle,
+} from "react-icons/bi";
+import { HiArrowLeft, HiCog, HiSelector } from "react-icons/hi";
 
-import Button from '@/components/ui/buttons/Button';
-import Link from 'next/link';
-import { useRouter } from 'next/router';
-import { BsFillPlusCircleFill } from 'react-icons/bs';
-import { createPagesServerClient } from '@supabase/auth-helpers-nextjs';
-import { Database } from '@/database.types';
-import { useQuery } from 'react-query';
-import fetchSchedulesByTrackId from '@/services/fetchScedulesByTrackId';
-import { ScheduleUpdateRequestBody } from '@/types';
-import { HiArrowRight } from 'react-icons/hi2';
-import { Loader } from '@/components/ui/Loader';
+import Button from "@/components/ui/buttons/Button";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { BsFillPlusCircleFill } from "react-icons/bs";
+import { createPagesServerClient } from "@supabase/auth-helpers-nextjs";
+import { Database } from "@/database.types";
+import { useQuery } from "react-query";
+import fetchSchedulesByTrackId from "@/services/fetchScedulesByTrackId";
+import { ScheduleUpdateRequestBody } from "@/types";
+import { HiArrowRight } from "react-icons/hi2";
+import { Loader } from "@/components/ui/Loader";
 
 type IdProp = {
   id: string;
@@ -28,7 +37,7 @@ export default function SchedulesDashboardPage() {
     isLoading,
     isError,
   } = useQuery<Joined<IdProp>[], Error>(
-    ['schedules', eventId],
+    ["schedules", eventId],
     () => fetchSchedulesByTrackId(trackId as string),
 
     {
@@ -43,7 +52,7 @@ export default function SchedulesDashboardPage() {
         query: { eventId: eventId, trackId: trackId, scheduleId: id },
       });
     } catch (error) {
-      console.error('Error fetching space details', error);
+      console.error("Error fetching space details", error);
     }
   };
 
@@ -57,22 +66,22 @@ export default function SchedulesDashboardPage() {
         query: { eventId: eventId, trackId: trackId },
       });
     } catch (error) {
-      console.error('Error fetching space details', error);
+      console.error("Error fetching space details", error);
     }
   };
   function formatDate(dateString: string | number | Date) {
     const date = new Date(dateString);
-    return new Intl.DateTimeFormat('en-US', {
-      year: 'numeric',
-      month: 'long',
-      day: 'numeric',
+    return new Intl.DateTimeFormat("en-US", {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
     }).format(date);
   }
   function formatTime(dateString: string | number | Date) {
     const date = new Date(dateString);
-    return new Intl.DateTimeFormat('en-US', {
-      hour: 'numeric',
-      minute: 'numeric',
+    return new Intl.DateTimeFormat("en-US", {
+      hour: "numeric",
+      minute: "numeric",
       hour12: true,
     }).format(date);
   }
@@ -108,7 +117,9 @@ export default function SchedulesDashboardPage() {
               <div className="w-28 h-28 bg-black rounded-lg"></div>
               <div className="flex flex-col gap-5 self-stretch">
                 <h2 className="text-4xl font-semibold">{trackTitle}</h2>
-                <h3 className="text-2xl leading-[1.2] opacity-70 font-bold">Schedules</h3>
+                <h3 className="text-2xl leading-[1.2] opacity-70 font-bold">
+                  Schedules
+                </h3>
               </div>
             </div>
             <div className="flex justify-between items-start self-stretch">
@@ -122,10 +133,18 @@ export default function SchedulesDashboardPage() {
               </Button>
 
               <div className="flex items-start gap-3">
-                <Button className="rounded-[40px] py-2.5 px-3.5 bg-bgPrimary border-none hover:bg-[#363636] duration-200 text-textSecondary hover:text-textSecondary" size="lg" leftIcon={HiSelector}>
+                <Button
+                  className="rounded-[40px] py-2.5 px-3.5 bg-bgPrimary border-none hover:bg-[#363636] duration-200 text-textSecondary hover:text-textSecondary"
+                  size="lg"
+                  leftIcon={HiSelector}
+                >
                   Sort
                 </Button>
-                <Button className="rounded-[40px] py-2.5 px-3.5 bg-bgPrimary border-none hover:bg-[#363636] duration-200 text-textSecondary hover:text-textSecondary" size="lg" leftIcon={HiCog}>
+                <Button
+                  className="rounded-[40px] py-2.5 px-3.5 bg-bgPrimary border-none hover:bg-[#363636] duration-200 text-textSecondary hover:text-textSecondary"
+                  size="lg"
+                  leftIcon={HiCog}
+                >
                   Select
                 </Button>
               </div>
@@ -145,21 +164,34 @@ export default function SchedulesDashboardPage() {
                         <div className="flex items-start gap-10">
                           <BiRadioCircle className="w-10 h-10" />
                           <div className="flex flex-col items-start gap-[10px]">
-                            <span className="text-[18px] font-semibold leading-[1.2]">{schedule.name}</span>
+                            <span className="text-[18px] font-semibold leading-[1.2]">
+                              {schedule.name}
+                            </span>
                             <div className="flex items-start self-stretch gap-6">
                               <span className="flex items-center p-1 gap-1 rounded-[10px] opacity-60 bg-[#FFFFFF10] white-space-nowrap overflow-hidden text-ellipsis">
                                 <BiCalendarAlt size={30} />
-                                <span className="ml-2">{formatDate(schedule?.date)}</span>
+                                <span className="ml-2">
+                                  {formatDate(schedule?.date)}
+                                </span>
                               </span>
                               <span className="flex items-center p-1 gap-1 rounded-[10px] opacity-60 bg-[#FFFFFF10] white-space-nowrap overflow-hidden text-ellipsis">
                                 <BiTimeFive size={30} />
-                                <span className="ml-2">{formatTime(schedule?.start_time)}</span>
+                                {schedule.start_time && (
+                                  <span className="ml-2">
+                                    {formatTime(schedule?.start_time)}
+                                  </span>
+                                )}
                               </span>
                             </div>
                           </div>
                         </div>
                         <div className="w-full">
-                          <Button variant="dark" className="bg-white/20 text-white/70 rounded-full" leftIcon={HiArrowRight} onClick={() => handleEnterTrack(schedule.id)}>
+                          <Button
+                            variant="dark"
+                            className="bg-white/20 text-white/70 rounded-full"
+                            leftIcon={HiArrowRight}
+                            onClick={() => handleEnterTrack(schedule.id)}
+                          >
                             Update Schedule
                           </Button>
                         </div>
@@ -191,7 +223,10 @@ export const getServerSideProps = async (ctx: any) => {
     };
 
   // get profile from session
-  const { data: profile, error } = await supabase.from('profile').select('*').eq('uuid', session.user.id);
+  const { data: profile, error } = await supabase
+    .from("profile")
+    .select("*")
+    .eq("uuid", session.user.id);
 
   return {
     props: {
