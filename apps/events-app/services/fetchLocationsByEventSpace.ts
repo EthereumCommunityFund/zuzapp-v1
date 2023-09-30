f9d93893cda255722c75ff8f25a17617dd42169f
davik20 <onyejivic@gmail.com>
2023-09-30 09:44:07 +0100

    update

--- apps/events-app/services/fetchLocationsByEventSpace.ts
+++ apps/events-app/services/fetchLocationsByEventSpace.ts
@@ -3,7 +3,7 @@ import { fetchLocationsByEventSpace } from '@/controllers';
 const fetchLocationsByEventSpaceId = async (id: string) => {
   try {
     const response = await fetchLocationsByEventSpace(id);
-    console.log(response, 'trackresponse');
+    // console.log(response, 'trackresponse');
     const data = response.data.data;
     return data;
   } catch (error) {
