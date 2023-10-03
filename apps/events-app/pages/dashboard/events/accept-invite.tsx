538ca947bcec5e0c220f3e34f242cd4db1374b66
davik20 <onyejivic@gmail.com>
2023-10-03 15:57:44 +0100

    fix build errors

--- apps/events-app/pages/dashboard/events/accept-invite.tsx
+++ apps/events-app/pages/dashboard/events/accept-invite.tsx
@@ -1,16 +1,16 @@
-import { EventInviteCard } from '@/components/templates/events/EventInviteCard';
-import { useState } from 'react';
-import router, { useRouter } from 'next/router';
-import { useUserPassportContext } from '@/context/PassportContext';
-import { useGlobalContext } from '@/context/GlobalContext';
-import { createPagesServerClient } from '@supabase/auth-helpers-nextjs';
-import { Database } from '@/database.types';
-import { updateInvite } from '@/controllers/invite.controller';
-import { useQuery } from 'react-query';
-import fetchInviteById from '@/services/fetchInviteById';
-import useEventDetails from '@/hooks/eventSpaceDetails';
-import { EventSpaceDetailsType } from '@/types';
-import { fetchEventSpaceById } from '@/services/fetchEventSpaceDetails';
+import { EventInviteCard } from "@/components/templates/events/EventInviteCard";
+import { useState } from "react";
+import router, { useRouter } from "next/router";
+import { useUserPassportContext } from "@/context/PassportContext";
+import { useGlobalContext } from "@/context/GlobalContext";
+import { createPagesServerClient } from "@supabase/auth-helpers-nextjs";
+import { Database } from "@/database.types";
+import { updateInvite } from "@/controllers/invite.controller";
+import { useQuery } from "react-query";
+import fetchInviteById from "@/services/fetchInviteById";
+import { EventSpaceDetailsType } from "@/types";
+import { fetchEventSpaceById } from "@/services/fetchEventSpaceDetails";
+import useCurrentEventSpace from "@/hooks/useCurrentEventSpace";
 
 export default function Invited() {
   const { signIn } = useUserPassportContext();
@@ -18,43 +18,38 @@ export default function Invited() {
   const [accepted, setAccepted] = useState(false);
   const router = useRouter();
   const { invite_id } = router.query;
-  const [eventId, setEventId] = useState('');
-
-  // const { eventSpace } = useEventDetails();
+  const [eventId, setEventId] = useState("");
 
   const {
     data: inviteDetails,
     isLoading,
     isError,
-  } = useQuery<any, Error>(['inviteDetails', invite_id], () => fetchInviteById(invite_id as string), {
-    enabled: !!invite_id,
-    onSuccess: (data) => {
-      console.log('invite', data);
-      setEventId(data?.event_space_id as string);
-      if (data.status === 'accepted') {
-        setAccepted(true);
-      }
-    },
-  });
-
-  const { data: eventSpace } = useQuery<EventSpaceDetailsType, Error>(
-    ['spaceDetails', eventId], // Query key
-    () => fetchEventSpaceById(eventId as string), // Query function
+  } = useQuery<any, Error>(
+    ["inviteDetails", invite_id],
+    () => fetchInviteById(invite_id as string),
     {
-      enabled: !!eventId,
-      refetchOnWindowFocus: false, // Only execute the query if eventId is available
+      enabled: !!invite_id,
+      onSuccess: (data) => {
+        console.log("invite", data);
+        setEventId(data?.event_space_id as string);
+        if (data.status === "accepted") {
+          setAccepted(true);
+        }
+      },
     }
   );
+
+  const { eventSpace } = useCurrentEventSpace();
 
   const handleUpdateInvite = async () => {
     if (!invite_id) return;
     setAccepted(true);
     try {
       await updateInvite(invite_id as string, {
-        status: 'accepted',
+        status: "accepted",
       });
     } catch (error) {
-      console.error('Error sending invite', error);
+      console.error("Error sending invite", error);
     }
   };
 
@@ -74,11 +69,14 @@ export default function Invited() {
           info="You need to login to accept this invitation to edit"
         />
       );
-    } else if (accepted || (inviteDetails && inviteDetails.status === 'accepted')) {
+    } else if (
+      accepted ||
+      (inviteDetails && inviteDetails.status === "accepted")
+    ) {
       return (
         <EventInviteCard
           spacename={eventSpace?.name as string}
-          buttonTitle={'My Event Spaces'}
+          buttonTitle={"My Event Spaces"}
           onButtonClick={handleButtonClick}
           buttonVariant="blue"
           info="Invite Accepted, Head to your Event Spaces to view your invited spaces"
@@ -86,12 +84,22 @@ export default function Invited() {
       );
     } else {
       return (
-        <EventInviteCard spacename={eventSpace?.name as string} onButtonClick={handleUpdateInvite} buttonTitle="Accept invite" buttonVariant="blue" info="Click the Button to Accept the invite" />
+        <EventInviteCard
+          spacename={eventSpace?.name as string}
+          onButtonClick={handleUpdateInvite}
+          buttonTitle="Accept invite"
+          buttonVariant="blue"
+          info="Click the Button to Accept the invite"
+        />
       );
     }
   };
 
-  return <div className="flex items-center justify-center">{renderEventInviteCard()}</div>;
+  return (
+    <div className="flex items-center justify-center">
+      {renderEventInviteCard()}
+    </div>
+  );
 }
 
 export const getServerSideProps = async (ctx: any) => {
@@ -109,7 +117,10 @@ export const getServerSideProps = async (ctx: any) => {
     };
 
   // get profile from session
-  const { data: profile, error } = await supabase.from('profile').select('*').eq('uuid', session.user.id);
+  const { data: profile, error } = await supabase
+    .from("profile")
+    .select("*")
+    .eq("uuid", session.user.id);
 
   return {
     props: {
