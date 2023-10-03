2a8a6e56695b8d2111375105675d832cd4d3248f
dev_waliba <donwaleyb@gmail.com>
2023-10-03 21:23:05 +0100

    minor ui fixes and code cleanup

--- apps/events-app/components/tracks/EditTrackForm.tsx
+++ apps/events-app/components/tracks/EditTrackForm.tsx
@@ -1,65 +1,40 @@
-import Link from "next/link";
-import { zodResolver } from "@hookform/resolvers/zod";
-import * as z from "zod";
-import {
-  Form,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import Button from "@/components/ui/buttons/Button";
-import { useForm } from "react-hook-form";
-import ImageUploadForm from "../templates/ImageUploadForm";
-import EditionButtons from "../ui/buttons/EditionButtons";
-import { CgClose } from "react-icons/cg";
-import { FaCircleArrowUp } from "react-icons/fa6";
-import IconButton from "../ui/buttons/IconButton";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "../ui/dialog";
-import { BsMap } from "react-icons/bs";
-import { useRef, useState } from "react";
-import DragAndDrop from "../ui/dragDrop";
-import { useRouter } from "next/router";
-import { useQuery, QueryClient } from "react-query";
-import { TrackUpdateRequestBody } from "@/types";
-import { fetchTrackById } from "@/services/fetchTrack";
-import { deleteTrack } from "@/controllers";
-import InputFieldLabel from "../ui/labels/inputFieldLabel";
-import EventDeatilsDescription1 from "../ui/labels/event-details-description-1";
-import Image from "next/image";
-import TextEditor from "../ui/TextEditor";
-import { toast } from "../ui/use-toast";
+import Link from 'next/link';
+import { zodResolver } from '@hookform/resolvers/zod';
+import * as z from 'zod';
+import { Form, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import Button from '@/components/ui/buttons/Button';
+import { useForm } from 'react-hook-form';
+import ImageUploadForm from '../templates/ImageUploadForm';
+import EditionButtons from '../ui/buttons/EditionButtons';
+import { CgClose } from 'react-icons/cg';
+import { FaCircleArrowUp } from 'react-icons/fa6';
+import IconButton from '../ui/buttons/IconButton';
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from '../ui/dialog';
+import { BsMap } from 'react-icons/bs';
+import { useRef, useState } from 'react';
+import DragAndDrop from '../ui/dragDrop';
+import { useRouter } from 'next/router';
+import { useQuery, QueryClient } from 'react-query';
+import { TrackUpdateRequestBody } from '@/types';
+import { fetchTrackById } from '@/services/fetchTrack';
+import { deleteTrack } from '@/controllers';
+import InputFieldLabel from '../ui/labels/inputFieldLabel';
+import EventDeatilsDescription1 from '../ui/labels/event-details-description-1';
+import Image from 'next/image';
+import TextEditor from '../ui/TextEditor';
+import { toast } from '../ui/use-toast';
 
 const trackSchema = z.object({
   name: z.string().min(2, {
-    message: "Track name is required.",
+    message: 'Track name is required.',
   }),
   description: z.string().min(10, {
-    message:
-      "Track description is required and must be at least 10 characters.",
+    message: 'Track description is required and must be at least 10 characters.',
   }),
-  // event_space_id: z.string().min(2, {
-  //   message: 'event_space_id is required',
-  // }),
-  // Add more validation for other fields if needed
 });
 
-export default function EditTrackForm({
-  onTrackSubmit,
-  trackDetails,
-}: {
-  onTrackSubmit: (values: z.infer<typeof trackSchema>) => void;
-  trackDetails: TrackUpdateRequestBody;
-}) {
+export default function EditTrackForm({ onTrackSubmit, trackDetails }: { onTrackSubmit: (values: z.infer<typeof trackSchema>) => void; trackDetails: TrackUpdateRequestBody }) {
   const router = useRouter();
   const { event_space_id, trackId } = router.query;
   console.log(trackDetails);
@@ -74,10 +49,7 @@ export default function EditTrackForm({
   };
 
   const handleRemoveImage = (index: number) => {
-    const updatedItems = [
-      ...payload.image_urls.slice(0, index),
-      ...payload.image_urls.slice(index + 1),
-    ];
+    const updatedItems = [...payload.image_urls.slice(0, index), ...payload.image_urls.slice(index + 1)];
     setPayload({ ...payload, image_urls: updatedItems });
   };
 
@@ -93,9 +65,9 @@ export default function EditTrackForm({
     const image = payload.image_urls[0];
     if (!image) {
       toast({
-        title: "Error",
-        description: "Select at least one image",
-        variant: "destructive",
+        title: 'Error',
+        description: 'Select at least one image',
+        variant: 'destructive',
       });
       return;
     }
@@ -113,10 +85,7 @@ export default function EditTrackForm({
 
   return (
     <Form {...form}>
-      <form
-        onSubmit={form.handleSubmit(onSubmitWithEnter)}
-        className="space-y-10 w-full"
-      >
+      <form onSubmit={form.handleSubmit(onSubmitWithEnter)} className="space-y-10 w-full">
         <FormField
           control={form.control}
           name="name"
@@ -147,30 +116,18 @@ export default function EditTrackForm({
         />
 
         <div className="flex flex-col items-center gap-[10px] self-stretch">
-          <InputFieldLabel name="Track Image Url" />
+          <InputFieldLabel name="Track Image" />
           <DragAndDrop payload={payload} setPayload={setPayload} />
           <EventDeatilsDescription1 name="We recommend using at least a 2160x1080px" />
         </div>
-        {payload.image_urls.length == 0 && (
-          <p className="text-sm text-btnRed">Select at least one image</p>
-        )}
+        {payload.image_urls.length == 0 && <p className="text-sm text-btnRed">Select at least one image</p>}
         {payload.image_urls.length > 0 && (
           <div className="flex space-x-3">
             {payload.image_urls.map((source, index) => (
               <div className="" key={index}>
                 <div className="rounded-[10px] w-[130px] h-[100px] bg-pagePrimary relative">
-                  <IconButton
-                    variant="dark"
-                    className="rounded-full absolute right-[-5px] top-[-5px] z-10 bg-gray-400"
-                    onClick={() => handleRemoveImage(index)}
-                    icon={CgClose}
-                  />
-                  <Image
-                    src={source as string}
-                    alt=""
-                    fill
-                    className="object-contain"
-                  />
+                  <IconButton variant="dark" className="rounded-full absolute right-[-5px] top-[-5px] z-10 bg-gray-400" onClick={() => handleRemoveImage(index)} icon={CgClose} />
+                  <Image src={source as string} alt="" fill className="object-contain" />
                 </div>
               </div>
             ))}
@@ -178,24 +135,10 @@ export default function EditTrackForm({
         )}
         <div className="flex justify-center pt-8">
           <div className="flex gap-[30px] w-full">
-            <Button
-              onClick={handleDeleteTrack}
-              className="rounded-full w-1/2 flex justify-center"
-              variant="quiet"
-              size="lg"
-              type="button"
-              leftIcon={CgClose}
-            >
+            <Button onClick={handleDeleteTrack} className="rounded-full w-1/2 flex justify-center" variant="quiet" size="lg" type="button" leftIcon={CgClose}>
               <span>Discard Track</span>
             </Button>
-            <Button
-              className="rounded-full w-1/2 flex justify-center"
-              variant="blue"
-              size="lg"
-              // type="submit"
-              onClick={() => form.handleSubmit(onSubmit)()}
-              leftIcon={FaCircleArrowUp}
-            >
+            <Button className="rounded-full w-1/2 flex justify-center" variant="blue" size="lg" onClick={() => form.handleSubmit(onSubmit)()} leftIcon={FaCircleArrowUp}>
               <span>Update Track</span>
             </Button>
           </div>
