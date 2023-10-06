d3692519ee111b448b61bef01ee4e30083d76582
dev_waliba <donwaleyb@gmail.com>
2023-10-06 04:18:01 +0100

    schedule list and quick access

---
+++ apps/events-app/components/schedules/ScheduleItemCard.tsx
@@ -0,0 +1,99 @@
+import { ScheduleUpdateRequestBody } from '@/types';
+import { BiCalendarAlt, BiRadioCircle, BiTimeFive } from 'react-icons/bi';
+import Button from '../ui/buttons/Button';
+import { HiArrowRight } from 'react-icons/hi';
+import { useQuery } from 'react-query';
+import fetchSchedulesByTrackId from '@/services/fetchScedulesByTrackId';
+import { useRouter } from 'next/router';
+
+type IdProp = {
+  id: string;
+};
+
+type Joined<T> = ScheduleUpdateRequestBody & T;
+
+export default function ScheduleItemCard() {
+  const router = useRouter();
+  const { event_space_id, trackId, track_title } = router.query;
+
+  const {
+    data: schedules,
+    isLoading,
+    isError,
+  } = useQuery<Joined<IdProp>[], Error>(
+    ['schedules', event_space_id],
+    () => fetchSchedulesByTrackId(trackId as string),
+
+    {
+      enabled: !!event_space_id,
+    }
+  );
+
+  const handleEnterSchedule = async (id: string) => {
+    try {
+      router.push({
+        pathname: `/dashboard/events/space/tracks/schedules/updateSchedule`,
+        query: {
+          event_space_id,
+          trackId,
+          scheduleId: id,
+          track_title: track_title,
+        },
+      });
+    } catch (error) {
+      console.error('Error fetching space details', error);
+    }
+  };
+
+  function formatDate(dateString: string | number | Date) {
+    const date = new Date(dateString);
+    return new Intl.DateTimeFormat('en-US', {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+    }).format(date);
+  }
+
+  function formatTime(dateString: string | number | Date) {
+    const date = new Date(dateString);
+    return new Intl.DateTimeFormat('en-US', {
+      hour: 'numeric',
+      minute: 'numeric',
+      hour12: true,
+    }).format(date);
+  }
+
+  return (
+    <div className="w-full">
+      {schedules && (
+        <div className="flex flex-col gap-[10px] w-full">
+          {schedules.map((schedule) => (
+            <div className="flex flex-col items-center justify-between gap-[10px]">
+              <div className="flex py-3 px-3.5 items-center justify-between gap-[364px] rounded-2xl border border-white border-opacity-10 bg-[#2E3131] w-full">
+                <div className="flex items-start gap-10">
+                  <BiRadioCircle className="w-10 h-10" />
+                  <div className="flex flex-col items-start gap-[10px]">
+                    <span className="text-[18px] font-semibold leading-[1.2]">{schedule.name}</span>
+                    <div className="flex items-start self-stretch gap-6">
+                      <span className="flex items-center p-1 gap-1 rounded-[10px] opacity-60 bg-[#FFFFFF10] white-space-nowrap overflow-hidden text-ellipsis">
+                        <BiCalendarAlt size={30} />
+                        <span className="ml-2">{formatDate(schedule?.date)}</span>
+                      </span>
+                      <span className="flex items-center p-1 gap-1 rounded-[10px] opacity-60 bg-[#FFFFFF10] white-space-nowrap overflow-hidden text-ellipsis">
+                        <BiTimeFive size={30} />
+                        <span className="ml-2">{formatTime(schedule?.start_time)} </span>
+                      </span>
+                    </div>
+                  </div>
+                </div>
+                <Button variant="dark" className="bg-white/20 text-white/70 rounded-full" leftIcon={HiArrowRight} onClick={() => handleEnterSchedule(schedule.id)}>
+                  Update Schedule
+                </Button>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
--- apps/events-app/components/ui/DropDownMenu.tsx
+++ apps/events-app/components/ui/DropDownMenu.tsx
@@ -1,42 +1,33 @@
-import { Fragment, useState } from 'react'
-import { Transition } from '@headlessui/react'
-import { Listbox } from '@headlessui/react'
-import { TbChevronDown } from 'react-icons/tb'
-import { DropDownMenuItemType } from '@/types'
-import { IconType } from 'react-icons'
+import { Fragment, useState } from 'react';
+import { Transition } from '@headlessui/react';
+import { Listbox } from '@headlessui/react';
+import { TbChevronDown } from 'react-icons/tb';
+import { DropDownMenuItemType } from '@/types';
+import { IconType } from 'react-icons';
+import { cn } from '@/lib/utils';
 
-
-
-
-type DropDownMenu = {
+export type DropDownMenu = {
   data: DropDownMenuItemType[];
   header: string;
   headerIcon?: any;
-}
+  multiple: boolean;
+  value: string | string[];
+  onChange: (newValue: string) => void;
+  className?: string;
+};
 
-export const List = (props: DropDownMenu) => {
-  const { data, header, headerIcon: HeaderIcon } = props;
-
-  const [selectedPeople, setSelectedPeople] = useState()
-
-  console.log(selectedPeople);
+export const DropDownMenu = (props: DropDownMenu) => {
+  const { data, header, headerIcon: HeaderIcon, multiple, value, onChange, className } = props;
 
   return (
     <>
-      <Listbox as={"div"} className={"w-72 relative"} value={selectedPeople} onChange={setSelectedPeople} multiple>
-        {/* <Listbox.Button>
-          {selectedPeople.map((person) => person.name).join(', ')}
-        </Listbox.Button> */}
+      <Listbox as={'div'} className={cn('w-full relative', className)} value={value} onChange={onChange} multiple={multiple}>
         <Listbox.Button className="relative w-full inline-flex justify-between item-center cursor-pointer rounded-2xl bg-slate-600 py-2 px-2 shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
-          <div className='flex gap-2 items-center font-semibold pl-2'>
+          <div className="flex gap-2 items-center font-semibold pl-2">
             {HeaderIcon && <HeaderIcon />}
             {header}
           </div>
-          <TbChevronDown
-            className="h-5 w-5 text-gray-40 font-extrabold"
-            aria-hidden="true"
-          />
-
+          <TbChevronDown className="h-5 w-5 text-gray-40 font-extrabold" aria-hidden="true" />
         </Listbox.Button>
         <Transition
           as={Fragment}
@@ -47,23 +38,12 @@ export const List = (props: DropDownMenu) => {
           leaveFrom="transform opacity-100 scale-100"
           leaveTo="transform opacity-0 scale-95"
         >
-          <Listbox.Options className={"absolute right-0 z-10 mt-2 w-full pb-2 bg-componentPrimary origin-top-right rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"}>
+          <Listbox.Options className={'absolute right-0 z-10 mt-2 w-full pb-2 bg-componentPrimary origin-top-right rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'}>
             {data.map((item, idx) => (
-              <Listbox.Option key={idx} value={item} className={"block pt-2 px-2 text-sm"}>
+              <Listbox.Option key={idx} value={item} className={'block pt-2 px-2 text-sm'}>
                 {({ selected }) => (
                   <>
-                    <span
-                      className={`relative block truncate rounded-2xl py-2 px-2 w-full hover:bg-slate-700 ${selected ? 'font-medium bg-slate-700' : 'font-normal'
-                        }`}
-                    >
-                      {item.name}
-                      {selected ? (
-                        <span className="absolute inset-y-0 right-1 flex items-center pl-3 text-white-600">
-                          {/* <XCircleIcon className="h-5 w-5" aria-hidden="true" /> */}222
-                        </span>
-                      ) : null}
-                    </span>
-
+                    <span className={`relative block truncate rounded-2xl py-2 px-2 w-full hover:bg-slate-700 ${selected ? 'font-medium bg-slate-700' : 'font-normal'}`}>{item.name}</span>
                   </>
                 )}
               </Listbox.Option>
@@ -72,5 +52,5 @@ export const List = (props: DropDownMenu) => {
         </Transition>
       </Listbox>
     </>
-  )
-}
+  );
+};
