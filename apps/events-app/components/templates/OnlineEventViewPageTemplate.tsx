161c520be0d2987e7dad7c9d2e892ae547295e47
MightyDevKnight <james@ecf.network>
2023-10-08 10:00:26 -0500

    Add responsiveness to online templates

--- apps/events-app/components/templates/OnlineEventViewPageTemplate.tsx
+++ apps/events-app/components/templates/OnlineEventViewPageTemplate.tsx
@@ -64,8 +64,8 @@ export default function OnlineEventViewPageTemplate({ eventSpace }: IOnlineEvent
             <img src={eventSpace.image_url} className="w-full pb-5 rounded-2xl" alt="" height={600} />
           </div>
           <div className="flex flex-col gap-2.5 pb-5 border-b-2 border-white/10 w-full p-5">
-            <div className="sm:flex sm:flex-col sm:gap-5 items-center justify-between w-full pb-5 lg:flex-row md:flex-row">
-              <div className="flex flex-col gap-3 w-full">
+            <div className="items-center justify-between w-full pb-5 sm:flex-col sm:gap-3 sm:flex md:flex md:flex-row">
+              <div className="flex flex-col gap-3 sm:w-full md:w-auto">
                 <div className="flex items-center gap-3 text-[#D7FFC4]/80">
                   <LockClosed />
                   <span className="font-bold">Zuzalu Residents & Invited Guests</span>
@@ -73,7 +73,7 @@ export default function OnlineEventViewPageTemplate({ eventSpace }: IOnlineEvent
                 <h2 className="font-semibold text-[30px]">{name}</h2>
                 <span className="text-white/80 font-bold">{tagline}</span>
               </div>
-              <Button variant="primaryGreen" size="lg" className="rounded-full sm:w-full lg:w-inherit md:w-auto justify-center" leftIcon={BsArrowRightCircleFill}>Apply to Event</Button>
+              <Button variant="primaryGreen" size="lg" className="rounded-full sm:w-full md:w-auto lg:w-auto justify-center" leftIcon={BsArrowRightCircleFill}>Apply to Event</Button>
             </div>
             <div className="flex gap-3 text-lg">
               <span className="rounded-full flex px-4 py-1 items-center gap-1 opacity-60 bg-[#FFFFFF10] font-bold">
