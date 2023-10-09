57297a7f6786cf3efdc632879156c061bceb34f3
MightyDevKnight <james@ecf.network>
2023-10-09 05:14:52 -0500

    Add blur to eventview subheader

--- apps/events-app/components/eventview/EventViewHeader.tsx
+++ apps/events-app/components/eventview/EventViewHeader.tsx
@@ -9,10 +9,10 @@ interface IEventViewHeader {
 export default function EventViewHeader(props: IEventViewHeader) {
   const { imgPath, name, tagline } = props;
   return (
-    <div className={"flex px-10 rounded-full gap-[10px] h-[60px] justify-between items-center lg:bg-inherit md:bg-inherit/80 lg:relative sm:fixed sm:bottom-0 w-full"}>
+    <div className={"flex px-10 lg:rounded-full gap-[10px] h-[60px] justify-between items-center lg:bg-inherit sm:backdrop-blur-sm sm:bg-white/10 sm:h-20 lg:relative sm:fixed sm:bottom-0 sm:z-[1000] w-full"}>
       <img src={imgPath} className="rounded-xl" width={90} height={90} alt="event" />
       <div className="flex flex-col gap-2 w-3/4">
-        <h2 className="font-bold text-2xl">{name}</h2>
+        <h2 className="font-bold text-2xl z-50">{name}</h2>
         <span className="opacity-70 text-[15px] lg:flex md:hidden">{tagline}</span>
       </div>
       <Button variant="primaryGreen" className="rounded-full w-[200px] justify-center text-[18px]">Apply to Event</Button>
