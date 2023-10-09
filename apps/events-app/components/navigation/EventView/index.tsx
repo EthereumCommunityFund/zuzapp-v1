e7b91a7cbaf745be5d47e8ae37fa67b804932610
MightyDevKnight <james@ecf.network>
2023-10-08 21:45:43 -0500

    Resolve merge conflicts

--- apps/events-app/components/navigation/EventView/index.tsx
+++ apps/events-app/components/navigation/EventView/index.tsx
@@ -1,13 +1,13 @@
-import Button from "@/components/ui/buttons/Button";
-import { eventViewRoutes } from "@/constant/routes";
-import { useGlobalContext } from "@/context/GlobalContext";
-import Link from "next/link";
-import { useRouter } from "next/router";
-import { MdOutlineModeEdit } from "react-icons/md";
-import { HiArrowLeft, HiOutlineMenuAlt1 } from "react-icons/hi";
+import Button from '@/components/ui/buttons/Button';
+import { eventViewRoutes } from '@/constant/routes';
+import { useGlobalContext } from '@/context/GlobalContext';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { MdOutlineModeEdit } from 'react-icons/md';
+import { HiArrowLeft, HiOutlineMenuAlt1 } from 'react-icons/hi';
 
-import React, { useEffect, useState } from "react";
-import { ArrowCircleLeft } from "@/components/ui/icons";
+import React, { useEffect, useState } from 'react';
+import { ArrowCircleLeft } from '@/components/ui/icons';
 
 export default function EventViewNavigation() {
   const [isClient, setIsClient] = useState(false);
@@ -16,7 +16,7 @@ export default function EventViewNavigation() {
   const { event_space_id } = router.query;
 
   if (!event_space_id) {
-    router.push("/404");
+    router.push('/404');
   }
   const [dashboardOpen, setDashboardOpen] = React.useState(true);
   const routes = eventViewRoutes;
@@ -26,74 +26,64 @@ export default function EventViewNavigation() {
   };
 
   const handleEditEvent = () => {
-    router.push(`/dashboard/eventview/?event_space_id=${event_space_id}`);
+    router.push(`/dashboard/events/space/details?event_space_id=${event_space_id}`);
   };
 
   const handleEditSchedules = () => {
-    router.push(`/dashboard/eventview/?event_space_id=${event_space_id}`);
+    router.push(`/dashboard/events/space/details?event_space_id=${event_space_id}`);
   };
 
   const handleBackToEvents = () => {
-    router.push("/dashboard/home");
+    router.push('/dashboard/home');
   };
 
   const handleEventRouteClick = (path: string) => {
     router.push({
       pathname: path,
-      query: { event_space_id }
-    })
-  }
+      query: { event_space_id },
+    });
+  };
 
   return (
     <>
       {/* Mobile menu button */}
       <div className="z-50 top-4 left-3 fixed">
-        <button
-          onClick={handleClick}
-          className="block md:hidden "
-          aria-label="Open Dashboard Menu"
-        >
+        <button onClick={handleClick} className="block md:hidden " aria-label="Open Dashboard Menu">
           <HiOutlineMenuAlt1 size={28} />
         </button>
       </div>
       {/* Overlay that can close the dashboard menu */}
-      <div
-        className={`md:hidden fixed inset-0 bg-black/10 z-10 ${dashboardOpen ? "block" : "hidden"
-          }`}
-        onClick={handleClick}
-      ></div>
+      <div className={`md:hidden fixed inset-0 bg-black/10 z-10 ${dashboardOpen ? 'block' : 'hidden'}`} onClick={handleClick}></div>
       <nav
         className={`z-50 dashboard-menu lg:w-[250px] md:w-full sm:w-full fixed flex flex-col lg:h-screen md:h-auto sm:h-auto border-r border-r-gray-800 bg-pagePrimary lg:py-10 md:pt-10 sm:pt-10 pl-10 transition-transform duration-300 ${dashboardOpen && "open"
           }`}
       >
         <div className="flex-1 flex flex-col gap-5 max-w-max">
           <div className="mt-10 flex-1">
-            <Button size="lg" variant="quiet-SM" className="lg:pb-10 md:pb-2 sm:pb-1 opacity-80" leftIcon={ArrowCircleLeft} onClick={handleBackToEvents}>Back to Events</Button>
+            <Button size="lg" variant="quiet-SM" className="lg:pb-10 md:pb-2 sm:pb-1 opacity-80" leftIcon={ArrowCircleLeft} onClick={handleBackToEvents}>
+              Back to Events
+            </Button>
             <div className="flex flex-col gap-3.5 lg:pb-10 md:pb-1">
               <span className="font-semibold md:hidden lg:contents sm:hidden">Navigate Event</span>
               <ul className="space-y-3 md:flex md:items-end lg:block sm:flex sm:items-end">
-                {routes.map((route, index) => (route.name &&
-                  <li
-                    key={index}
-                    className={`flex items-center font-bold space-x-2 py-1 px-3 opacity-70 rounded-xl hover:bg-white/20 md:hover:border-b duration-200 ${router.pathname.includes(route.path)
-                      }`}
-                  >
-                    {route.icon && <route.icon size={30} />}
-                    <Link href={`${route.path}?event_space_id=${event_space_id}`} >
-                      {route.name}
-                    </Link>
-                  </li>
-                ))}
+                {routes.map(
+                  (route, index) =>
+                    route.name && (
+                      <li
+                        key={index}
+                        className={`flex items-center font-bold space-x-2 py-1 px-3 opacity-70 rounded-xl hover:bg-white/20 md:hover:border-b duration-200 ${router.pathname.includes(route.path)}`}
+                      >
+                        {route.icon && <route.icon size={30} />}
+                        <Link href={`${route.path}?event_space_id=${event_space_id}`}>{route.name}</Link>
+                      </li>
+                    )
+                )}
               </ul>
             </div>
             {router.pathname.includes("dashboard/eventview/tracks") && (
               <div className="flex-col gap-3 rounded-md p-2 bg-black font-bold sm:hidden lg:flex">
                 <h2>Organizer</h2>
-                <Button
-                  variant="ghost"
-                  className="p-2 w-full gap-3 text-base"
-                  onClick={handleEditEvent}
-                >
+                <Button variant="ghost" className="p-2 w-full gap-3 text-base" onClick={handleEditEvent}>
                   <MdOutlineModeEdit />
                   <span>Edit Event</span>
                 </Button>
@@ -102,11 +92,7 @@ export default function EventViewNavigation() {
             {router.pathname.includes("dashboard/eventview/allschedules") && (
               <div className="flex-col gap-3 rounded-md p-2 bg-black font-bold sm:hidden lg:flex">
                 <h2>Organizer</h2>
-                <Button
-                  variant="ghost"
-                  className="p-2 w-full gap-3 text-base"
-                  onClick={handleEditSchedules}
-                >
+                <Button variant="ghost" className="p-2 w-full gap-3 text-base" onClick={handleEditSchedules}>
                   <MdOutlineModeEdit />
                   <span>Edit Schedules</span>
                 </Button>
