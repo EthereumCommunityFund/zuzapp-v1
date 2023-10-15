75f3d6467d2813b951b4bb288720ed4cdced0865
davik20 <onyejivic@gmail.com>
2023-10-15 20:00:09 +0100

    update staging

--- apps/events-app/pages/_app.tsx
+++ apps/events-app/pages/_app.tsx
@@ -23,7 +23,7 @@ const App = ({ Component, pageProps }: { Component: any; pageProps: any }) => {
       "eventSpaces",
       "invitedSpaces",
       "publishedEventSpaces",
-      "currentEventSpace",
+      // "currentEventSpace",
     ];
 
     queryKeys.forEach((key) => {
--- apps/events-app/pages/dashboard/eventview/allschedules/index.tsx
+++ apps/events-app/pages/dashboard/eventview/allschedules/index.tsx
@@ -47,7 +47,7 @@ const categoryList: DropDownMenuItemType[] = [
 export default function EventViewTracksAlleSchedulesPage() {
   const router = useRouter();
   const { event_space_id, trackId, track_title } = router.query;
-  const { eventSpace } = useEventDetails();
+  const { eventSpace, isLoading: isLoadingSpace } = useEventDetails();
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [schedules, setSchedules] = useState<ScheduleDetailstype[]>();
 
@@ -80,97 +80,104 @@ export default function EventViewTracksAlleSchedulesPage() {
   }, [isLoading]);
 
   return (
-    <div className="flex gap-4 lg:flex-row mt-5 lg:mt-0 pb-24 lg:pb-0 sm:flex-col-reverse lg:bg-pagePrimary md:bg-componentPrimary">
-      <div className="flex flex-col lg:w-2/3 sm:w-full pb-30 lg:pb-0 gap-5">
-        <EventViewHeader
-          imgPath={eventSpace?.image_url as string}
-          name={eventSpace?.name as string}
-          tagline={eventSpace?.tagline as string}
-        />
-        <div className="flex flex-col gap-2.5 lg:px-9 md:px-5">
-          <div className="bg-componentPrimary rounded-2xl lg:px-5 lg:pt-8">
-            <div>
-              <Dialog>
-                <DialogTrigger asChild>
-                  <Button
-                    variant="blue"
-                    size="lg"
-                    className="rounded-full sm:w-full lg:w-fit justify-center"
-                    leftIcon={BiPlusCircle}
-                  >
-                    Add a Schedule
-                  </Button>
-                </DialogTrigger>
-                {
-                  <DialogContent className="md:w-3/5 md:h-3/5 overflow-x-auto sm:w-3/4">
-                    <ScheduleEditForm
-                      title={"Add"}
-                      isFromAllSchedules={true}
-                      trackId={trackId as string}
-                      updateIsLoading={updateIsLoading}
-                    />
-                  </DialogContent>
-                }
-              </Dialog>
-            </div>
-            {isLoading ? (
-              <Loader />
-            ) : (
-              <div className=" p-2.5 gap-[10px] flex flex-col overflow-hidden rounded-[10px] pb-36">
-                {schedules &&
-                  schedules.map((schedule, id) => (
-                    <UserFacingTrack
-                      key={id}
-                      onClick={() =>
-                        handleItemClick(
-                          schedule.id,
-                          schedule.track_id as string
-                        )
-                      }
-                      scheduleData={schedule}
-                      scheduleId={schedule.id}
-                    />
-                  ))}
+    <>
+      {isLoadingSpace ? (
+        <Loader />
+      ) : (
+        <div className="flex gap-4 lg:flex-row mt-5 lg:mt-0 pb-24 lg:pb-0 sm:flex-col-reverse lg:bg-pagePrimary md:bg-componentPrimary">
+          <div className="flex flex-col lg:w-2/3 sm:w-full pb-30 lg:pb-0 gap-5">
+            <EventViewHeader
+              imgPath={eventSpace?.image_url as string}
+              name={eventSpace?.name as string}
+              tagline={eventSpace?.tagline as string}
+            />
+
+            <div className="flex flex-col gap-2.5 lg:px-9 md:px-5">
+              <div className="bg-componentPrimary rounded-2xl lg:px-5 lg:pt-8">
+                <div>
+                  <Dialog>
+                    <DialogTrigger asChild>
+                      <Button
+                        variant="blue"
+                        size="lg"
+                        className="rounded-full sm:w-full lg:w-fit justify-center"
+                        leftIcon={BiPlusCircle}
+                      >
+                        Add a Schedule
+                      </Button>
+                    </DialogTrigger>
+                    {
+                      <DialogContent className="md:w-3/5 md:h-3/5 overflow-x-auto sm:w-3/4">
+                        <ScheduleEditForm
+                          title={"Add"}
+                          isFromAllSchedules={true}
+                          trackId={trackId as string}
+                          updateIsLoading={updateIsLoading}
+                        />
+                      </DialogContent>
+                    }
+                  </Dialog>
+                </div>
+                {isLoading ? (
+                  <Loader />
+                ) : (
+                  <div className=" p-2.5 gap-[10px] flex flex-col overflow-hidden rounded-[10px] pb-36">
+                    {schedules &&
+                      schedules.map((schedule, id) => (
+                        <UserFacingTrack
+                          key={id}
+                          onClick={() =>
+                            handleItemClick(
+                              schedule.id,
+                              schedule.track_id as string
+                            )
+                          }
+                          scheduleData={schedule}
+                          scheduleId={schedule.id}
+                        />
+                      ))}
+                  </div>
+                )}
               </div>
-            )}
+            </div>
+          </div>
+          <div className="lg:w-1/4 sm:w-full flex lg:flex-col gap-5 lg:fixed lg:right-0 min-w-fit lg:mr-10">
+            <h2 className="p-3.5 gap-[10px] font-bold text-xl sm:hidden lg:flex">
+              Schedules: Sort & Filter
+            </h2>
+            <div className="flex lg:flex-col md:flex-row sm:flex-col w-full p-2.5 md:gap-5 sm:gap-3 text-sm">
+              <DropDownMenu
+                data={categoryList}
+                header={"Select Categories"}
+                headerIcon={SelectCategories}
+                multiple={true}
+                value={""}
+                headerClassName={"rounded-full bg-borderPrimary"}
+                optionsClassName={""}
+              />
+              <DropDownMenu
+                data={categoryList}
+                header={"Select Dates"}
+                headerIcon={Calendar}
+                multiple={true}
+                value={""}
+                headerClassName={"rounded-full bg-borderPrimary"}
+                optionsClassName={""}
+              />
+              <DropDownMenu
+                data={categoryList}
+                header={"Select Location"}
+                headerIcon={SelectLocation}
+                multiple={true}
+                value={""}
+                headerClassName={"rounded-full bg-borderPrimary"}
+                optionsClassName={""}
+              />
+            </div>
           </div>
         </div>
-      </div>
-      <div className="lg:w-1/4 sm:w-full flex lg:flex-col gap-5 lg:fixed lg:right-0 min-w-fit lg:mr-10">
-        <h2 className="p-3.5 gap-[10px] font-bold text-xl sm:hidden lg:flex">
-          Schedules: Sort & Filter
-        </h2>
-        <div className="flex lg:flex-col md:flex-row sm:flex-col w-full p-2.5 md:gap-5 sm:gap-3 text-sm">
-          <DropDownMenu
-            data={categoryList}
-            header={"Select Categories"}
-            headerIcon={SelectCategories}
-            multiple={true}
-            value={""}
-            headerClassName={"rounded-full bg-borderPrimary"}
-            optionsClassName={""}
-          />
-          <DropDownMenu
-            data={categoryList}
-            header={"Select Dates"}
-            headerIcon={Calendar}
-            multiple={true}
-            value={""}
-            headerClassName={"rounded-full bg-borderPrimary"}
-            optionsClassName={""}
-          />
-          <DropDownMenu
-            data={categoryList}
-            header={"Select Location"}
-            headerIcon={SelectLocation}
-            multiple={true}
-            value={""}
-            headerClassName={"rounded-full bg-borderPrimary"}
-            optionsClassName={""}
-          />
-        </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 }
 
