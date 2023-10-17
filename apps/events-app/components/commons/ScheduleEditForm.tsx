59302809a099a64ff834a19b1297aefbc48cc6e6
MightyDevKnight <james@ecf.network>
2023-10-17 20:31:13 +0100

    Fix form style issue in eventview

--- apps/events-app/components/commons/ScheduleEditForm.tsx
+++ apps/events-app/components/commons/ScheduleEditForm.tsx
@@ -452,7 +452,7 @@ export default function ScheduleEditForm({ title, isFromAllSchedules, scheduleId
                         <RadioGroup
                           onValueChange={field.onChange}
                           //  defaultValue={field.value}
-                          className="flex flex-col md:flex-row justify-between"
+                          className="flex flex-col md:flex-row"
                           {...field}
                         >
                           <FormItem className="flex items-center space-x-3 space-y-0 p-3 hover:bg-btnPrimaryGreen/20 rounded-md focus:bg-btnPrimaryGreen/20">
