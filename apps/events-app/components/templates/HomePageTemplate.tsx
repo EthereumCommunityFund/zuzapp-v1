6d2a65bd42dc7750e868f63e4efb42bc6173533c
dev_waliba <donwaleyb@gmail.com>
2023-10-20 04:45:32 +0100

    banner

--- apps/events-app/components/templates/HomePageTemplate.tsx
+++ apps/events-app/components/templates/HomePageTemplate.tsx
@@ -1,47 +1,39 @@
-import { useUserPassportContext } from "@/context/PassportContext";
-import Button from "../ui/buttons/Button";
-import Image from "next/image";
-import Link from "next/link";
-import { BsCalendar2Fill } from "react-icons/bs";
-import { HiLockClosed } from "react-icons/hi";
-import { useGlobalContext } from "@/context/GlobalContext";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "../ui/dialog";
-import { useRouter } from "next/router";
-import { EventSpaceDetailsType, EventTypes } from "@/types";
-import { useQuery } from "react-query";
-import { fetchPublishedEventSpaces } from "@/services/fetchPublishedEvents";
-import { Loader } from "../ui/Loader";
-import { arrayFromLength } from "@/lib/helper";
-import { EventTemplateSkeleton } from "../commons/EventTemplateSkeleton";
-import { HomePageTemplateSkeleton } from "../commons/HomePageTemplateSkeleton";
-import { useEventSpace, useEventSpaces } from "@/context/EventSpaceContext";
-import { useState } from "react";
-import { Input } from "../ui/input";
-import { ArrowCircleLeft, ArrowCircleRight, ArrowLeft } from "../ui/icons";
-import { updateUsername } from "@/controllers/profile.controllers";
-import banner from "@/public/images/zuconnectbanner.png";
-import CustomCarousel from "../ui/CustomCarousel";
+import { useUserPassportContext } from '@/context/PassportContext';
+import Button from '../ui/buttons/Button';
+import Image from 'next/image';
+import Link from 'next/link';
+import { BsCalendar2Fill } from 'react-icons/bs';
+import { HiLockClosed } from 'react-icons/hi';
+import { useGlobalContext } from '@/context/GlobalContext';
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from '../ui/dialog';
+import { useRouter } from 'next/router';
+import { EventSpaceDetailsType, EventTypes } from '@/types';
+import { useQuery } from 'react-query';
+import { fetchPublishedEventSpaces } from '@/services/fetchPublishedEvents';
+import { Loader } from '../ui/Loader';
+import { arrayFromLength } from '@/lib/helper';
+import { EventTemplateSkeleton } from '../commons/EventTemplateSkeleton';
+import { HomePageTemplateSkeleton } from '../commons/HomePageTemplateSkeleton';
+import { useEventSpace, useEventSpaces } from '@/context/EventSpaceContext';
+import { useState } from 'react';
+import { Input } from '../ui/input';
+import { ArrowCircleLeft, ArrowCircleRight, ArrowLeft } from '../ui/icons';
+import { updateUsername } from '@/controllers/profile.controllers';
+import banner1 from '@/public/images/zuconnectbanner.png';
+import CustomCarousel from '../ui/CustomCarousel';
 
 export const sampleEvents = [
   {
     type: EventTypes.InPerson,
-    name: "Zu Connect",
-    description: "A Popup Village of Innovation in the Heart of Istanbul",
-    date: "October 8 - October 20",
+    name: 'Zu Connect',
+    description: 'A Popup Village of Innovation in the Heart of Istanbul',
+    date: 'October 8 - October 20',
   },
   {
     type: EventTypes.Online,
-    name: "Zuzalu Meetings",
-    description: "Here we post our Town Halls and other events!",
-    date: "Recuring",
+    name: 'Zuzalu Meetings',
+    description: 'Here we post our Town Halls and other events!',
+    date: 'Recuring',
   },
 ];
 
@@ -56,13 +48,12 @@ export default function HomePageTemplate() {
   const { signIn } = useUserPassportContext();
   const { isAuthenticated, user } = useGlobalContext();
   const router = useRouter();
-  const [userName, setUsername] = useState<string>("");
+  const [userName, setUsername] = useState<string>('');
   const { firstLogin } = router.query;
   const [dialogContent, setDialogContent] = useState<DialogContent>({
     title: "Welcome to Zuzalu, let's get your name!",
-    description:
-      "Type in a username. Does not have to be your real name. You can also change your username later",
-    buttonLabel: "Continue",
+    description: 'Type in a username. Does not have to be your real name. You can also change your username later',
+    buttonLabel: 'Continue',
   });
 
   const { eventSpaceList, setEventSpaceList } = useEventSpace();
@@ -79,11 +70,11 @@ export default function HomePageTemplate() {
     isLoading,
     isError,
   } = useQuery<EventSpaceDetailsType[], Error>(
-    ["publishedEventSpaces"], // Query key
+    ['publishedEventSpaces'], // Query key
     () => fetchPublishedEventSpaces({ page: 1, limit: 10 }),
     {
       onSuccess: (data) => {
-        console.log("HomePageTemplate Event Spaces:", data);
+        console.log('HomePageTemplate Event Spaces:', data);
         setEventSpaceList(data);
       },
     }
@@ -91,30 +82,30 @@ export default function HomePageTemplate() {
 
   function formatDate(dateString: string | number | Date) {
     const date = new Date(dateString);
-    return new Intl.DateTimeFormat("en-US", {
-      year: "numeric",
-      month: "long",
-      day: "numeric",
+    return new Intl.DateTimeFormat('en-US', {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
     }).format(date);
   }
 
   const handleDialogButton = async () => {
-    console.log("userName", userName);
+    console.log('userName', userName);
     try {
       const res = await updateUsername({ username: userName });
       console.log(res);
       setDialogContent({
         title: `Welcome ${userName}`,
-        description: "Now, head to explore Zuzalu & community events!",
-        buttonLabel: "Complete",
+        description: 'Now, head to explore Zuzalu & community events!',
+        buttonLabel: 'Complete',
       });
     } catch (error) {
-      console.error("Error updating username", error);
+      console.error('Error updating username', error);
     }
   };
-  console.log(banner)
+  console.log(banner1);
 
-  const slides = [banner.src, banner.src];
+  const slides = [banner1.src, banner1.src];
 
   return (
     <div className="md:w-5/6 w-[95%] mx-auto ">
@@ -204,42 +195,23 @@ export default function HomePageTemplate() {
               >
                 <div className="flex flex-col md:flex-row md:space-x-3 md:items-center">
                   <div>
-                    <img
-                      src={
-                        event.image_url
-                          ? event.image_url
-                          : `/images/black-img.png`
-                      }
-                      className="rounded-xl w-full md:max-w-[150px] md:max-h-[120px] h-44"
-                      alt="Event"
-                      width={150}
-                      height={120}
-                    />
+                    <img src={event.image_url ? event.image_url : `/images/black-img.png`} className="rounded-xl w-full md:max-w-[150px] md:max-h-[120px] h-44" alt="Event" width={150} height={120} />
                   </div>
                   <div className="space-y-2 space-x-0 mt-2 md:mt-0">
                     <h4 className="text-2xl font-bold">{event.name}</h4>
-                    <h2 className="text-base font-semibold opacity-70 font-inter">
-                      {event.tagline}
-                    </h2>
+                    <h2 className="text-base font-semibold opacity-70 font-inter">{event.tagline}</h2>
                     <div className="flex gap-2 flex-wrap">
                       <p className="flex items-center text-xs md:text-sm text-white/60 bg-white/10 rounded-full py-2 px-3 w-fit font-semibold">
-                        <BsCalendar2Fill className="mr-2 text-sm md:text-base" />{" "}
-                        {formatDate(event?.start_date)} -{" "}
-                        {formatDate(event?.end_date)}
+                        <BsCalendar2Fill className="mr-2 text-sm md:text-base" /> {formatDate(event?.start_date)} - {formatDate(event?.end_date)}
                       </p>
                       <p className="flex items-center text-xs md:text-sm text-white/60 bg-white/10 rounded-full py-2 px-3 w-fit font-semibold">
-                        <HiLockClosed className="mr-2 text-sm md:text-base" />{" "}
-                        Resident Only
+                        <HiLockClosed className="mr-2 text-sm md:text-base" /> Resident Only
                       </p>
                     </div>
                   </div>
                 </div>
                 <div className="mt-3 md:mt-0">
-                  <Button
-                    size="lg"
-                    className="rounded-full w-full flex items-center justify-center md:w-auto"
-                    onClick={() => event.id && handleButtonClick(event.id)}
-                  >
+                  <Button size="lg" className="rounded-full w-full flex items-center justify-center md:w-auto" onClick={() => event.id && handleButtonClick(event.id)}>
                     View Event
                   </Button>
                 </div>
@@ -280,29 +252,16 @@ export default function HomePageTemplate() {
         <Dialog open={true}>
           <DialogContent className="sm:max-w-[425px]">
             <DialogHeader>
-              <DialogTitle className="text-2xl">
-                {dialogContent?.title}
-              </DialogTitle>
-              <DialogDescription className="text-lg font-bold">
-                {dialogContent?.description}
-              </DialogDescription>
+              <DialogTitle className="text-2xl">{dialogContent?.title}</DialogTitle>
+              <DialogDescription className="text-lg font-bold">{dialogContent?.description}</DialogDescription>
             </DialogHeader>
             <DialogFooter className="pt-5">
-              <Input
-                placeholder="Type your username"
-                className={`bg-black text-white`}
-                value={userName}
-                onChange={(e) => setUsername(e.target.value)}
-              />
+              <Input placeholder="Type your username" className={`bg-black text-white`} value={userName} onChange={(e) => setUsername(e.target.value)} />
               <Button
                 variant={`${userName.length ? `strongerGreen` : `primary`}`}
                 className="w-full flex items-center justify-center rounded-3xl py-2 h-full bg-dark text-lg md:text-base"
                 leftIcon={ArrowCircleRight}
-                onClick={
-                  dialogContent.buttonLabel === "Continue"
-                    ? handleDialogButton
-                    : () => router.push("/dashboard/home")
-                }
+                onClick={dialogContent.buttonLabel === 'Continue' ? handleDialogButton : () => router.push('/dashboard/home')}
               >
                 {dialogContent?.buttonLabel}
               </Button>
