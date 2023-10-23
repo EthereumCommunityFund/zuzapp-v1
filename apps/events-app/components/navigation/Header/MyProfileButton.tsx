2c9dda82a03ce6d0dee47bc9a01bc31fa0888713
dev_waliba <donwaleyb@gmail.com>
2023-10-23 04:29:52 +0100

    update profile button types

--- apps/events-app/components/navigation/Header/MyProfileButton.tsx
+++ apps/events-app/components/navigation/Header/MyProfileButton.tsx
@@ -6,27 +6,29 @@ import { useRouter } from 'next/router';
 import { v4 } from 'uuid';
 import { useState } from 'react';
 import { User } from '@/components/ui/icons';
-import Avvvatars from 'avvvatars-react'
+import Avvvatars from 'avvvatars-react';
+
+import { ReactNode } from 'react';
 
 interface DropDownMenuItem {
-  icon: IconType;
+  icon: ReactNode;
   label: string;
   path: string;
 }
 
 const MyProfileDropDownMenu: DropDownMenuItem[] = [
   {
-    icon: <User/>,
+    icon: <User />,
     label: 'My Profile',
     path: '/dashboard/user-profile',
   },
   {
-    icon: <BsFillTicketFill/>,
+    icon: <BsFillTicketFill />,
     label: 'My RSVPs',
     path: '/dashboard/events/space/tracks/addtrack',
   },
   {
-    icon: <BsArrowRightSquare/>,
+    icon: <BsArrowRightSquare />,
     label: 'Sign Out',
     path: '/dashboard/events/space/tracks/addtrack',
   },
@@ -47,20 +49,20 @@ const MyProfileButton: React.FC<MyProfileButtonType> = (props: MyProfileButtonTy
       <DropdownMenu.Root>
         <DropdownMenu.Trigger>
           <div className="rounded-[60px] flex items-center gap-3 border border-[#333435] pr-3 pl-1 py-1 bg-white/10 hover:bg-white/20 duration-200">
-            <Avvvatars value="drivenfast" style="shape"/>
+            <Avvvatars value="drivenfast" style="shape" />
             <span className="hidden md:inline-block">{userName}</span>
           </div>
         </DropdownMenu.Trigger>
 
         <DropdownMenu.Content className="flex top-[10px] py-3 flex-col self-stretch bg-[#383B3B] rounded-[10px] border border-white/10 backdrop-blur-[20px] mt-4 mr-4 z-50 w-68">
           <div className="flex gap-5 items-center px-5 py-3">
-            <Avvvatars value="drivenfast" style="shape" size={40}/>
+            <Avvvatars value="drivenfast" style="shape" size={40} />
             <div className="flex flex-col">
               <p className="text-lg font-semibold text-white">{userName}</p>
-                <div className="text-textSecondary text-sm flex items-center gap-1">
-                    <small className="inline-block bg-green-400 w-1.5 h-1.5 rounded-full"></small>
-                    <span>Zupass Connected</span>
-                </div>
+              <div className="text-textSecondary text-sm flex items-center gap-1">
+                <small className="inline-block bg-green-400 w-1.5 h-1.5 rounded-full"></small>
+                <span>Zupass Connected</span>
+              </div>
             </div>
           </div>
           <DropdownMenu.Separator className="border-[0.5px] border-white/20 w-full my-2" />
@@ -98,7 +100,6 @@ const MyProfileButton: React.FC<MyProfileButtonType> = (props: MyProfileButtonTy
               <span>Blog </span>
             </div>
           </footer>
-
         </DropdownMenu.Content>
       </DropdownMenu.Root>
     </>
