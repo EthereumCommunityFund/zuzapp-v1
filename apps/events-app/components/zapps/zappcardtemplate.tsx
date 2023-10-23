af1c95fc0de52e17c1b3687f972368df73bc75b5
MightyDevKnight <james@ecf.network>
2023-10-23 23:50:07 +0100

    Fix ui in zapps page

--- apps/events-app/components/zapps/zappcardtemplate.tsx
+++ apps/events-app/components/zapps/zappcardtemplate.tsx
@@ -4,34 +4,28 @@ import Button from "../ui/buttons/Button";
 import { BiCode } from "react-icons/bi";
 
 export default function ZappCardComponentTemplate(props: ZappCardTemplate) {
-  const { imgURL, appTitle, appDescription, appContents } = props;
+  const { imgURL, appTitle, appTagLine, appContents } = props;
   return (
-    <div className="flex flex-col p-2.5 bg-componentPrimary rounded-md md:w-80 sm:w-full h-80 hover:bg-itemHover hover:cursor-pointer">
-      {imgURL ?
-        <img className="rounded-lg h-[120px] w-auto" src={imgURL} alt="item-card" /> :
-        <div className="flex h-32 items-center justify-center">
-          <Label className="text-white text-xl">Your App</Label>
-        </div>
-      }
-      <div className="flex flex-col gap-3.5">
+    <div className="flex flex-col p-2.5 bg-componentPrimary rounded-2xl md:w-72 sm:w-full h-80 hover:bg-itemHover hover:cursor-pointer gap-3.5">
+      <img className="rounded-[10px] h-[120px] w-full object-cover" src={imgURL ? `${imgURL}` : `/images/zapps/new.png`} alt="item-card" />
+      <div className="flex flex-col gap-3.5 w-full">
         <div className="flex flex-col gap-2.5">
-          <Label className="text-white text-lg font-bold">
+          <Label className="text-white text-xl font-bold">
             {appTitle}
           </Label>
           <Label className="text-white/70 text-sm font-bold">
-            {appDescription}
+            {appTagLine}
           </Label>
         </div>
         <div className="flex gap-2.5">
           {appContents ?
             appContents.map((appContent) => {
               return (
-                <Label className="py-1 px-2.5 bg-itemBgPrimary text-white/60 text-sm rounded-xl">{appContent}</Label>
+                <Label className="py-1 px-2.5 bg-white/10 text-white/60 text-[13px] rounded-[10px] font-semibold">{appContent}</Label>
               )
             }) :
-            <Button variant='quiet-SM' leftIcon={BiCode} className="rounded-md w-full justify-center" >Start Building</Button>
+            <Button variant='quiet-SM' leftIcon={BiCode} className="rounded-2xl w-full justify-center bg-white/10" >Start Building</Button>
           }
-
         </div>
       </div>
     </div>
