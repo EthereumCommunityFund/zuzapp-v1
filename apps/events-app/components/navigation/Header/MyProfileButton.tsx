32f25c5406355a3a6d79f398a01081058f6aa3c1
dev_waliba <donwaleyb@gmail.com>
2023-10-23 08:33:56 +0100

    update username for avatar

--- apps/events-app/components/navigation/Header/MyProfileButton.tsx
+++ apps/events-app/components/navigation/Header/MyProfileButton.tsx
@@ -49,14 +49,14 @@ const MyProfileButton: React.FC<MyProfileButtonType> = (props: MyProfileButtonTy
       <DropdownMenu.Root>
         <DropdownMenu.Trigger>
           <div className="rounded-[60px] flex items-center gap-3 border border-[#333435] pr-3 pl-1 py-1 bg-white/10 hover:bg-white/20 duration-200">
-            <Avvvatars value="drivenfast" style="shape" />
+            <Avvvatars value={userName} style="shape" />
             <span className="hidden md:inline-block">{userName}</span>
           </div>
         </DropdownMenu.Trigger>
 
         <DropdownMenu.Content className="flex top-[10px] py-3 flex-col self-stretch bg-[#383B3B] rounded-[10px] border border-white/10 backdrop-blur-[20px] mt-4 mr-4 z-50 w-68">
           <div className="flex gap-5 items-center px-5 py-3">
-            <Avvvatars value="drivenfast" style="shape" size={40} />
+            <Avvvatars value={userName} style="shape" size={40} />
             <div className="flex flex-col">
               <p className="text-lg font-semibold text-white">{userName}</p>
               <div className="text-textSecondary text-sm flex items-center gap-1">
