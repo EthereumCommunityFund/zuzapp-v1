25b28e6cee59f568921c09841d1c479f38b43f0a
MightyDevKnight <james@ecf.network>
2023-10-24 12:04:19 +0100

    Fix styles issues in itemcard

--- apps/events-app/components/resources/ResourcesItemCard.tsx
+++ apps/events-app/components/resources/ResourcesItemCard.tsx
@@ -1,6 +1,7 @@
 import { ResourceItemCard } from "@/types";
 import { Label } from "../ui/label";
 import { ArrowTopRightOnSquare } from "../ui/icons";
+import Button from "../ui/buttons/Button";
 
 
 export default function ResourcesItemCard({ title, tagLine, link }: ResourceItemCard) {
@@ -10,8 +11,10 @@ export default function ResourcesItemCard({ title, tagLine, link }: ResourceItem
         <Label className="text-xl font-bold text-white">{title}</Label>
         <div className="flex flex-col gap-5">
           <Label className="opacity-80 text-white/80">{tagLine}</Label>
-          <a className="flex gap-2.5 opacity-50">
-            <ArrowTopRightOnSquare /><span>{link}</span>
+          {/* <Button variant="quiet-SM" className="items-center p-0 break-words" leftIcon={ArrowTopRightOnSquare}>{link}</Button> */}
+          <a className="flex gap-2.5 opacity-50 w-full">
+            <ArrowTopRightOnSquare />
+            <span className="break-all">{link}</span>
           </a>
         </div>
       </div>
