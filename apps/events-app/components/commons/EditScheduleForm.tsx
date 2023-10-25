47031ae67120624c6f1be808fe343f70e25cf96b
dev_waliba <donwaleyb@gmail.com>
2023-10-25 19:09:38 +0100

    form fixes

--- apps/events-app/components/commons/EditScheduleForm.tsx
+++ apps/events-app/components/commons/EditScheduleForm.tsx
@@ -140,10 +140,12 @@ export default function EditScheduleForm({ title, isQuickAccess, scheduleId, tra
           message: 'You need to select a valid date for this Session.',
         }
       ),
-    end_date: z.date({
-      required_error: 'You need to select a valid date for this event.',
-      invalid_type_error: 'You need to select a valid date for this event.',
-    }),
+    end_date: z
+      .date({
+        required_error: 'You need to select a valid date for this event.',
+        invalid_type_error: 'You need to select a valid date for this event.',
+      })
+      .optional(),
     description: z.string().min(10, {
       message: 'Description is required and must be a minimum of 5',
     }),
@@ -195,14 +197,16 @@ export default function EditScheduleForm({ title, isQuickAccess, scheduleId, tra
       });
       return;
     }
-    const endDate = dayjs(values.end_date);
-    const startDate = dayjs(values.date);
+    if (frequency === 'everyday' || frequency === 'weekly') {
+      const endDate = dayjs(values.end_date);
+      const startDate = dayjs(values.date);
 
-    if (endDate.isBefore(startDate)) {
-      form.setError('end_date', {
-        message: 'End date cannot be earlier than start date',
-      });
-      return;
+      if (endDate.isBefore(startDate)) {
+        form.setError('end_date', {
+          message: 'End date cannot be earlier than start date',
+        });
+        return;
+      }
     }
     const updatedOrganizers = (schedule.organizers as any).map((user: any) => {
       if (user.name) {
@@ -592,7 +596,7 @@ export default function EditScheduleForm({ title, isQuickAccess, scheduleId, tra
                           <div className="flex flex-col gap-[14px] items-start self-stretch w-full">
                             <span className="text-lg opacity-70 self-stretch">End Date</span>
 
-                            <CustomDatePicker defaultDate={undefined} selectedDate={field.value} handleDateChange={field.onChange} {...field} />
+                            <CustomDatePicker defaultDate={undefined} selectedDate={field.value || null} handleDateChange={field.onChange} {...field} />
 
                             <h3 className="opacity-70 h-3 font-normal text-[10px] leading-3">Click & Select or type in a date</h3>
                             <FormMessage />
