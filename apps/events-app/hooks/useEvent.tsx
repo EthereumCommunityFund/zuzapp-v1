c42a990371cac4a32f99dc8ef6e05eda3f37d5dd
dev_waliba <donwaleyb@gmail.com>
2023-10-25 07:51:24 +0100

    accept invite error and success messages

---
+++ apps/events-app/hooks/useEvent.tsx
@@ -0,0 +1,29 @@
+// create custom hook for fetching event details
+
+import { fetchEventSpaceById } from '@/services/fetchEventSpaceDetails';
+import { EventSpaceDetailsType } from '@/types';
+import { isError } from 'joi';
+import { useRouter } from 'next/router';
+import { useQuery } from 'react-query';
+
+const useEventDetatils = (event_space_id: string) => {
+  const {
+    data: eventSpace,
+    isLoading,
+    isError,
+  } = useQuery<EventSpaceDetailsType, Error>(
+    ['currentEventSpace', event_space_id], // Query key
+    () => fetchEventSpaceById(event_space_id as string),
+
+    {
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
+      onSuccess: (data) => {
+        console.log('selectedEventSpace Event Spaces:', data);
+      },
+    }
+  );
+  return { eventSpace, isLoading, isError };
+};
+
+export default useEventDetatils;
--- apps/events-app/pages/dashboard/events/accept-invite.tsx
+++ apps/events-app/pages/dashboard/events/accept-invite.tsx
@@ -1,16 +1,27 @@
-import { EventInviteCard } from "@/components/templates/events/EventInviteCard";
-import { useState } from "react";
-import router, { useRouter } from "next/router";
-import { useUserPassportContext } from "@/context/PassportContext";
-import { useGlobalContext } from "@/context/GlobalContext";
-import { createPagesServerClient } from "@supabase/auth-helpers-nextjs";
-import { Database } from "@/database.types";
-import { updateInvite } from "@/controllers/invite.controller";
-import { useQuery } from "react-query";
-import fetchInviteById from "@/services/fetchInviteById";
-import { EventSpaceDetailsType } from "@/types";
-import { fetchEventSpaceById } from "@/services/fetchEventSpaceDetails";
-import useCurrentEventSpace from "@/hooks/useCurrentEventSpace";
+import { EventInviteCard } from '@/components/templates/events/EventInviteCard';
+import { useState } from 'react';
+import router, { useRouter } from 'next/router';
+import { useUserPassportContext } from '@/context/PassportContext';
+import { useGlobalContext } from '@/context/GlobalContext';
+import { createPagesServerClient } from '@supabase/auth-helpers-nextjs';
+import { Database } from '@/database.types';
+import { updateInvite } from '@/controllers/invite.controller';
+import { useQuery } from 'react-query';
+import fetchInviteById from '@/services/fetchInviteById';
+import { EventSpaceDetailsType } from '@/types';
+import { fetchEventSpaceById } from '@/services/fetchEventSpaceDetails';
+import useCurrentEventSpace from '@/hooks/useCurrentEventSpace';
+import useEventDetatils from '@/hooks/useEvent';
+import { toast } from '@/components/ui/use-toast';
+import { Loader } from '@/components/ui/Loader';
+
+interface AxiosErrorResponse {
+  response?: {
+    data?: {
+      message?: string;
+    };
+  };
+}
 
 export default function Invited() {
   const { signIn } = useUserPassportContext();
@@ -18,38 +29,46 @@ export default function Invited() {
   const [accepted, setAccepted] = useState(false);
   const router = useRouter();
   const { invite_id } = router.query;
-  const [eventId, setEventId] = useState("");
+  const [eventId, setEventId] = useState('');
 
   const {
     data: inviteDetails,
     isLoading,
     isError,
-  } = useQuery<any, Error>(
-    ["inviteDetails", invite_id],
-    () => fetchInviteById(invite_id as string),
-    {
-      enabled: !!invite_id,
-      onSuccess: (data) => {
-        console.log("invite", data);
-        setEventId(data?.event_space_id as string);
-        if (data.status === "accepted") {
-          setAccepted(true);
-        }
-      },
-    }
-  );
+  } = useQuery<any, Error>(['inviteDetails', invite_id], () => fetchInviteById(invite_id as string), {
+    enabled: !!invite_id,
+    onSuccess: (data) => {
+      console.log('invite', data);
+      setEventId(data?.event_space_id as string);
+      if (data.status === 'accepted') {
+        setAccepted(true);
+      }
+    },
+  });
 
-  const { eventSpace } = useCurrentEventSpace();
+  const { eventSpace, isLoading: eventLoader } = useEventDetatils(eventId as string);
 
   const handleUpdateInvite = async () => {
     if (!invite_id) return;
-    setAccepted(true);
     try {
       await updateInvite(invite_id as string, {
-        status: "accepted",
+        status: 'accepted',
+      });
+      setAccepted(true);
+      toast({
+        title: 'Invite accepted successfully',
       });
     } catch (error) {
-      console.error("Error sending invite", error);
+      console.error('Error accepting invite', error);
+      setAccepted(false);
+
+      const axiosError = error as AxiosErrorResponse;
+      const errorMessage = axiosError.response?.data?.message || 'Error accepting invite';
+
+      toast({
+        title: errorMessage,
+        variant: 'destructive',
+      });
     }
   };
 
@@ -66,20 +85,19 @@ export default function Invited() {
           spacename={eventSpace?.name as string}
           buttonTitle="Connect Passport"
           buttonVariant="quiet"
+          loading={eventLoader}
           info="You need to login to accept this invitation to edit"
         />
       );
-    } else if (
-      accepted ||
-      (inviteDetails && inviteDetails.status === "accepted")
-    ) {
+    } else if (accepted || (inviteDetails && inviteDetails.status === 'accepted')) {
       return (
         <EventInviteCard
           spacename={eventSpace?.name as string}
-          buttonTitle={"My Event Spaces"}
+          buttonTitle={'My Event Spaces'}
           onButtonClick={handleButtonClick}
           buttonVariant="blue"
           info="Invite Accepted, Head to your Event Spaces to view your invited spaces"
+          loading={eventLoader}
         />
       );
     } else {
@@ -90,16 +108,13 @@ export default function Invited() {
           buttonTitle="Accept invite"
           buttonVariant="blue"
           info="Click the Button to Accept the invite"
+          loading={eventLoader}
         />
       );
     }
   };
 
-  return (
-    <div className="flex items-center justify-center">
-      {renderEventInviteCard()}
-    </div>
-  );
+  return <div className="flex items-center justify-center">{renderEventInviteCard()}</div>;
 }
 
 export const getServerSideProps = async (ctx: any) => {
