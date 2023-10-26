2426e982b94bf5953a1526148075dfcfbb9396c7
dev_waliba <donwaleyb@gmail.com>
2023-10-26 16:49:50 +0100

    update session cards

--- apps/events-app/components/ui/DatePicker.tsx
+++ apps/events-app/components/ui/DatePicker.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
+import React, { useState } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 interface DatePickerProps {
   selectedDate: Date | null; // Changed to allow null
@@ -9,35 +9,15 @@ interface DatePickerProps {
 }
 
 function range(start: number, end: number, step: number): number[] {
-  return Array.from(
-    { length: Math.floor((end - start) / step) + 1 },
-    (_, i) => start + i * step
-  );
+  return Array.from({ length: Math.floor((end - start) / step) + 1 }, (_, i) => start + i * step);
 }
 
-const CustomDatePicker: React.FC<DatePickerProps> = ({
-  selectedDate,
-  handleDateChange,
-  defaultDate,
-}) => {
+const CustomDatePicker: React.FC<DatePickerProps> = ({ selectedDate, handleDateChange, defaultDate }) => {
   const [startDate, setStartDate] = useState<Date | null>(selectedDate);
 
   const years = range(2021, new Date().getFullYear() + 1, 1);
 
-  const months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
+  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
   return (
     <div className="flex w-full rounded-lg py-2.5 pr-3 pl-2.5 bg-inputField gap-2.5 items-center border border-white/10 border-opacity-10">
@@ -52,37 +32,23 @@ const CustomDatePicker: React.FC<DatePickerProps> = ({
         }}
         value={defaultDate}
         dateFormat="dd-MM-yyyy"
-        renderCustomHeader={({
-          date,
-          changeYear,
-          changeMonth,
-          decreaseMonth,
-          increaseMonth,
-        }) => (
+        renderCustomHeader={({ date, changeYear, changeMonth, decreaseMonth, increaseMonth }) => (
           <div
             style={{
               margin: 10,
-              display: "flex",
-              justifyContent: "center",
+              display: 'flex',
+              justifyContent: 'center',
             }}
           >
             {/* <button onClick={decreaseMonth}>&lt;</button> */}
-            <select
-              title="year"
-              value={date.getFullYear()}
-              onChange={(e) => changeYear(Number(e.target.value))}
-            >
+            <select title="year" value={date.getFullYear()} onChange={(e) => changeYear(Number(e.target.value))}>
               {years.map((year) => (
                 <option key={year} value={year}>
                   {year}
                 </option>
               ))}
             </select>
-            <select
-              title="year"
-              value={months[date.getMonth()]}
-              onChange={(e) => changeMonth(months.indexOf(e.target.value))}
-            >
+            <select title="year" value={months[date.getMonth()]} onChange={(e) => changeMonth(months.indexOf(e.target.value))}>
               {months.map((month) => (
                 <option key={month} value={month}>
                   {month}
--- apps/events-app/components/ui/Speaker.tsx
+++ apps/events-app/components/ui/Speaker.tsx
@@ -7,7 +7,7 @@ interface IProps {
 export default function Speaker(props: IProps) {
   const { title } = props;
   return (
-    <div className="flex gap-2.5 py-1 px-2 rounded-[28px] bg-itemBgPrimary items-center w-fit">
+    <div className="flex gap-2.5 py-1 px-2 rounded-[28px] bg-trackItemHover items-center w-fit">
       <Avvvatars value={title} style="shape" size={20} />
       <p>{title}</p>
     </div>
