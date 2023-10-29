35cefaa98e8a4e45c75e109880943594edeeee0e
davik20 <onyejivic@gmail.com>
2023-10-29 21:45:53 +0100

    test build

--- apps/events-app/src/axiosInstance.ts
+++ apps/events-app/src/axiosInstance.ts
@@ -2,40 +2,40 @@ import axios, { AxiosInstance } from "axios";
 import Router from "next/router";
 
 const axiosInstance: AxiosInstance = axios.create({
-  // baseURL: 'https://zuzapp-v1-events-app-delta.vercel.app', // replace with your API endpoint
-  baseURL: "http://localhost:3001",
-  headers: {
-    "Content-Type": "application/json",
-  },
+    // baseURL: 'https://zuzapp-v1-events-app-delta.vercel.app', // replace with your API endpoint
+    baseURL: "zuzapp-test-v1-events-app-delta.vercel.app",
+    headers: {
+        "Content-Type": "application/json",
+    },
 });
 
 // Response interceptor
 axiosInstance.interceptors.response.use(
-  (response) => {
-    // Do something with the response data
-    return response;
-  },
-  (error) => {
-    if (error.response) {
-      switch (error.response.status) {
-        case 401:
-          Router.push("/");
-          break;
-        // case 403:
+    (response) => {
+        // Do something with the response dat
+        return response;
+    },
+    (error) => {
+        if (error.response) {
+            switch (error.response.status) {
+                case 401:
+                    Router.push("/");
+                    break;
+                // case 403:
 
-        //     alert('Forbidden. You do not have permission.');
-        //     break;
-        // case 500:
+                //     alert('Forbidden. You do not have permission.');
+                //     break;
+                // case 500:
 
-        //     alert('Server error. Please try again later.');
-        //     break;
-        // default:
+                //     alert('Server error. Please try again later.');
+                //     break;
+                // default:
 
-        //     alert('Something went wrong.');
-      }
+                //     alert('Something went wrong.');
+            }
+        }
+        return Promise.reject(error);
     }
-    return Promise.reject(error);
-  }
 );
 
 export default axiosInstance;
