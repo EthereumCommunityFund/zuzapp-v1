2ae33ac6c7c5421e38aa2229d75bd64dfb84ef41
dev_waliba <donwaleyb@gmail.com>
2023-10-30 03:47:53 +0100

    upcoming fix

--- apps/events-app/components/templates/SessionViewPageTemplate.tsx
+++ apps/events-app/components/templates/SessionViewPageTemplate.tsx
@@ -1,23 +1,23 @@
-import EventViewHeader from "@/components/eventview/EventViewHeader";
-import UserFacingTrack from "@/components/ui/UserFacingTrack";
-import Button from "@/components/ui/buttons/Button";
-import { ScheduleDetailstype } from "@/types";
-import { createPagesServerClient } from "@supabase/auth-helpers-nextjs";
-import { useRouter } from "next/router";
-import React, { Fragment, useEffect, useRef, useState } from "react";
-import { BiPlusCircle } from "react-icons/bi";
-import { QueryClient } from "react-query";
-import { EventSpaceDetailsType } from "@/types";
-import { Loader } from "@/components/ui/Loader";
-import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
-import fetchSchedulesByEvenSpaceId from "@/services/fetchScheduleByEventSpace";
-import AddScheduleForm from "@/components/commons/AddScheduleForm";
-import { useGlobalContext } from "@/context/GlobalContext";
-import { Listbox, Transition } from "@headlessui/react";
+import EventViewHeader from '@/components/eventview/EventViewHeader';
+import UserFacingTrack from '@/components/ui/UserFacingTrack';
+import Button from '@/components/ui/buttons/Button';
+import { ScheduleDetailstype } from '@/types';
+import { createPagesServerClient } from '@supabase/auth-helpers-nextjs';
+import { useRouter } from 'next/router';
+import React, { Fragment, useEffect, useRef, useState } from 'react';
+import { BiPlusCircle } from 'react-icons/bi';
+import { QueryClient } from 'react-query';
+import { EventSpaceDetailsType } from '@/types';
+import { Loader } from '@/components/ui/Loader';
+import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog';
+import fetchSchedulesByEvenSpaceId from '@/services/fetchScheduleByEventSpace';
+import AddScheduleForm from '@/components/commons/AddScheduleForm';
+import { useGlobalContext } from '@/context/GlobalContext';
+import { Listbox, Transition } from '@headlessui/react';
 
-import ToggleSwitch from "../commons/ToggleSwitch";
-import { TbChevronDown } from "react-icons/tb";
-import { convertToTurkeyTimeAsDate, toTurkeyTime } from "@/utils";
+import ToggleSwitch from '../commons/ToggleSwitch';
+import { TbChevronDown } from 'react-icons/tb';
+import { convertToTurkeyTimeAsDate, toTurkeyTime } from '@/utils';
 
 interface ISessionViewPageTemplate {
   event_space_id: string;
@@ -25,22 +25,16 @@ interface ISessionViewPageTemplate {
   eventSpace: EventSpaceDetailsType;
 }
 
-export default function SessionViewPageTemplate({
-  event_space_id,
-  trackId,
-  eventSpace,
-}: ISessionViewPageTemplate) {
+export default function SessionViewPageTemplate({ event_space_id, trackId, eventSpace }: ISessionViewPageTemplate) {
   const router = useRouter();
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [schedules, setSchedules] = useState<ScheduleDetailstype[]>([]);
-  const [filteredSchedules, setFilteredSchedules] = useState<
-    ScheduleDetailstype[]
-  >([]);
+  const [filteredSchedules, setFilteredSchedules] = useState<ScheduleDetailstype[]>([]);
   const lastTrackRef = useRef<HTMLDivElement>(null);
   const [isUpcoming, setIsUpcoming] = useState<boolean>(true);
   const [selectedTracks, setSelectedTracks] = useState<any[]>([]);
 
-  const groupedEvents = schedules?.forEach((schedule) => { });
+  const groupedEvents = schedules?.forEach((schedule) => {});
   const { isAuthenticated, user } = useGlobalContext();
   const handleItemClick = (scheduleId: string, trackId?: string) => {
     router.push({
@@ -53,17 +47,38 @@ export default function SessionViewPageTemplate({
     setIsLoading(newState);
   };
 
-  const fetchSchedules = async () => {
-    const response: ScheduleDetailstype[] = await fetchSchedulesByEvenSpaceId(
-      event_space_id as string
-    );
-    const filter: ScheduleDetailstype[] = response.filter((schedule) =>
-      isUpcoming
-        ? new Date(convertToTurkeyTimeAsDate(schedule.date)).getTime() > new Date().getTime()
-        : new Date(convertToTurkeyTimeAsDate(schedule.date)).getTime() < new Date().getTime()
-    );
+  const filterEventsByTime = (events: ScheduleDetailstype[], isUpcoming: boolean): ScheduleDetailstype[] => {
+    const now = new Date();
+    return events.filter((event) => {
+      let eventDate = new Date(convertToTurkeyTimeAsDate(event.date));
+      const endDate = new Date(convertToTurkeyTimeAsDate(event.end_date));
+      let isEventInFuture = false;
 
-    setFilteredSchedules(filter);
+      if (event.schedule_frequency) {
+        do {
+          if (eventDate >= now) {
+            isEventInFuture = true;
+            break;
+          }
+          if (event.schedule_frequency === 'everyday') {
+            eventDate.setDate(eventDate.getDate() + 1);
+          } else if (event.schedule_frequency === 'weekly') {
+            eventDate.setDate(eventDate.getDate() + 7);
+          }
+        } while (eventDate <= endDate);
+      } else {
+        isEventInFuture = eventDate >= now;
+      }
+
+      return isUpcoming ? isEventInFuture : !isEventInFuture;
+    });
+  };
+
+  const fetchSchedules = async () => {
+    const response: ScheduleDetailstype[] = await fetchSchedulesByEvenSpaceId(event_space_id as string);
+    const filteredEvents = filterEventsByTime(response, isUpcoming);
+
+    setFilteredSchedules(filteredEvents);
     setSchedules(response);
     setIsLoading(false);
   };
@@ -71,18 +86,8 @@ export default function SessionViewPageTemplate({
   const handleIsUpcoming = (newFilter: boolean) => {
     setIsLoading(true);
     const selectedTrackIds = selectedTracks.map((item) => item.id);
-    const filter: ScheduleDetailstype[] = schedules.filter((schedule) =>
-      newFilter
-        ? new Date(convertToTurkeyTimeAsDate(schedule.date)).getTime() > new Date().getTime()
-        : new Date(convertToTurkeyTimeAsDate(schedule.date)).getTime() < new Date().getTime()
-    );
-    const filteredByTracks =
-      selectedTrackIds.length > 0
-        ? filter.filter(
-          (schedule) =>
-            schedule.track_id && selectedTrackIds.includes(schedule.track_id)
-        )
-        : filter;
+    const filteredEvents = filterEventsByTime(schedules, newFilter);
+    const filteredByTracks = selectedTrackIds.length > 0 ? filteredEvents.filter((schedule) => schedule.track_id && selectedTrackIds.includes(schedule.track_id)) : filteredEvents;
     setFilteredSchedules(filteredByTracks);
     setIsUpcoming(newFilter);
     setIsLoading(false);
@@ -91,18 +96,10 @@ export default function SessionViewPageTemplate({
   const handleTrackSelect = (newSelectedTracks: any[]) => {
     const selectedTrackIds = newSelectedTracks.map((item) => item.id);
     const filter: ScheduleDetailstype[] = schedules.filter((schedule) =>
-      isUpcoming
-        ? new Date(convertToTurkeyTimeAsDate(schedule.date)).getTime() > new Date().getTime()
-        : new Date(convertToTurkeyTimeAsDate(schedule.date)).getTime() < new Date().getTime()
+      isUpcoming ? new Date(convertToTurkeyTimeAsDate(schedule.date)).getTime() > new Date().getTime() : new Date(convertToTurkeyTimeAsDate(schedule.date)).getTime() < new Date().getTime()
     );
 
-    const filteredByTracks =
-      selectedTrackIds.length > 0
-        ? filter.filter(
-          (schedule) =>
-            schedule.track_id && selectedTrackIds.includes(schedule.track_id)
-        )
-        : filter;
+    const filteredByTracks = selectedTrackIds.length > 0 ? filter.filter((schedule) => schedule.track_id && selectedTrackIds.includes(schedule.track_id)) : filter;
     setFilteredSchedules(filteredByTracks);
     setSelectedTracks(newSelectedTracks);
   };
@@ -143,10 +140,10 @@ export default function SessionViewPageTemplate({
     const frequency = schedule.schedule_frequency;
 
     do {
-      const formattedDate = date.toLocaleDateString("en-US", {
-        year: "numeric",
-        month: "long",
-        day: "numeric",
+      const formattedDate = date.toLocaleDateString('en-US', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
       });
 
       if (!groupedSchedules[formattedDate]) {
@@ -155,15 +152,14 @@ export default function SessionViewPageTemplate({
 
       const newSchedule = {
         ...schedule,
-        repeating:
-          !isFirstEvent && (frequency === "everyday" || frequency === "weekly"),
+        repeating: !isFirstEvent && (frequency === 'everyday' || frequency === 'weekly'),
       } as ScheduleDetailstype & { repeating: boolean };
 
       groupedSchedules[formattedDate].push(newSchedule);
 
-      if (frequency === "everyday") {
+      if (frequency === 'everyday') {
         date.setDate(date.getDate() + 1);
-      } else if (frequency === "weekly") {
+      } else if (frequency === 'weekly') {
         date.setDate(date.getDate() + 7);
       } else {
         break;
@@ -173,40 +169,24 @@ export default function SessionViewPageTemplate({
     } while (date <= end_date);
   });
 
-
   return (
     <>
       <div className="flex gap-4 lg:flex-row lg:mt-0 pb-24 lg:py-0 sm:pt-3 sm:px-3 sm:flex-col-reverse lg:bg-pagePrimary md:bg-componentPrimary">
         <div className="flex flex-col lg:w-2/3 sm:w-full pb-30 lg:pb-0 gap-5">
-          <EventViewHeader
-            imgPath={eventSpace?.image_url as string}
-            name={eventSpace?.name as string}
-            tagline={eventSpace?.tagline as string}
-          />
+          <EventViewHeader imgPath={eventSpace?.image_url as string} name={eventSpace?.name as string} tagline={eventSpace?.tagline as string} />
           <div className="flex flex-col gap-2.5 lg:px-1 md:px-1">
             <div className="bg-componentPrimary rounded-2xl lg:px-2 lg:pt-8">
               {isAuthenticated && (
                 <div className="px-4">
                   <Dialog>
                     <DialogTrigger asChild>
-                      <Button
-                        variant="blue"
-                        size="lg"
-                        className="rounded-full sm:w-full lg:w-fit justify-center"
-                        leftIcon={BiPlusCircle}
-                      >
+                      <Button variant="blue" size="lg" className="rounded-full sm:w-full lg:w-fit justify-center" leftIcon={BiPlusCircle}>
                         Add a Session
                       </Button>
                     </DialogTrigger>
                     {
                       <DialogContent className="md:w-3/5 md:h-3/5 overflow-x-auto sm:w-3/4">
-                        <AddScheduleForm
-                          title={"Add"}
-                          isQuickAccess={true}
-                          trackId={trackId as string}
-                          updateIsLoading={updateIsLoading}
-                          event_space_id={event_space_id as string}
-                        />
+                        <AddScheduleForm title={'Add'} isQuickAccess={true} trackId={trackId as string} updateIsLoading={updateIsLoading} event_space_id={event_space_id as string} />
                       </DialogContent>
                     }
                   </Dialog>
@@ -221,32 +201,17 @@ export default function SessionViewPageTemplate({
                       {Object.keys(groupedSchedules).map((date, idx) => {
                         return (
                           <>
-                            <div
-                              key={idx}
-                              className="text-center border-b-2 p-3 mt-10 border-borderPrimary"
-                            >
+                            <div key={idx} className="text-center border-b-2 p-3 mt-10 border-borderPrimary">
                               <span className="text-lg font-normal w-full">
-                                {new Date(date).toLocaleDateString("en-US", {
-                                  year: "numeric",
-                                  month: "long",
-                                  day: "numeric",
+                                {new Date(date).toLocaleDateString('en-US', {
+                                  year: 'numeric',
+                                  month: 'long',
+                                  day: 'numeric',
                                 })}
                               </span>
                             </div>
                             {groupedSchedules[date].map((schedule, idx) => {
-                              return (
-                                <UserFacingTrack
-                                  key={idx}
-                                  scheduleId={schedule.id}
-                                  scheduleData={schedule}
-                                  onClick={() =>
-                                    handleItemClick(
-                                      schedule.id,
-                                      schedule.track_id as string
-                                    )
-                                  }
-                                />
-                              );
+                              return <UserFacingTrack key={idx} scheduleId={schedule.id} scheduleData={schedule} onClick={() => handleItemClick(schedule.id, schedule.track_id as string)} />;
                             })}
                           </>
                         );
@@ -259,35 +224,19 @@ export default function SessionViewPageTemplate({
           </div>
         </div>
         <div className="lg:w-1/4 sm:w-full lg:pt-24 lg:flex-col gap-5 lg:fixed lg:right-0 min-w-fit lg:mr-10 lg:mt-[-100px]">
-          <h2 className="p-3.5 gap-[10px] font-bold text-xl sm:hidden lg:flex">
-            Sessions: Sort & Filter
-          </h2>
-          <ToggleSwitch
-            isUpcoming={isUpcoming}
-            handleIsUpcoming={handleIsUpcoming}
-          />
+          <h2 className="p-3.5 gap-[10px] font-bold text-xl sm:hidden lg:flex">Sessions: Sort & Filter</h2>
+          <ToggleSwitch isUpcoming={isUpcoming} handleIsUpcoming={handleIsUpcoming} />
           <div className="flex lg:flex-col md:flex-row sm:flex-col w-full p-2.5 md:gap-5 sm:gap-3 text-sm">
-            <Listbox
-              as={"div"}
-              className={"w-full relative"}
-              value={selectedTracks}
-              multiple
-              onChange={(newSelectedTracks) =>
-                handleTrackSelect(newSelectedTracks)
-              }
-            >
+            <Listbox as={'div'} className={'w-full relative'} value={selectedTracks} multiple onChange={(newSelectedTracks) => handleTrackSelect(newSelectedTracks)}>
               <Listbox.Button
                 className={
-                  "relative w-full inline-flex justify-between item-center cursor-pointer bg-trackItemHover border border-borderSecondary py-2 px-2 shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm rounded-xl"
+                  'relative w-full inline-flex justify-between item-center cursor-pointer bg-trackItemHover border border-borderSecondary py-2 px-2 shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm rounded-xl'
                 }
               >
                 <div className="flex gap-2 items-center font-semibold pl-2">
                   <span>Select Tracks</span>
                 </div>
-                <TbChevronDown
-                  className="h-5 w-5 text-gray-40 font-extrabold"
-                  aria-hidden="true"
-                />
+                <TbChevronDown className="h-5 w-5 text-gray-40 font-extrabold" aria-hidden="true" />
               </Listbox.Button>
               <Transition
                 as={Fragment}
@@ -298,27 +247,13 @@ export default function SessionViewPageTemplate({
                 leaveFrom="transform opacity-100 scale-100"
                 leaveTo="transform opacity-0 scale-95"
               >
-                <Listbox.Options
-                  className={
-                    "absolute right-0 z-10 mt-2 w-full pb-2 bg-componentPrimary origin-top-right rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-                  }
-                >
+                <Listbox.Options className={'absolute right-0 z-10 mt-2 w-full pb-2 bg-componentPrimary origin-top-right rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'}>
                   {eventSpace.tracks.map((item, idx) => (
-                    <Listbox.Option
-                      key={idx}
-                      value={item}
-                      className={"block pt-2 px-2 text-sm"}
-                    >
+                    <Listbox.Option key={idx} value={item} className={'block pt-2 px-2 text-sm'}>
                       {({ selected }) => (
                         <>
-                          <span
-                            className={`relative block truncate rounded-2xl py-2 cursor-pointer px-2 w-full hover:bg-itemHover ${selected
-                              ? "font-medium bg-slate-700"
-                              : "font-normal"
-                              }`}
-                          >
-                            {item.name.charAt(0).toUpperCase() +
-                              item.name.slice(1)}
+                          <span className={`relative block truncate rounded-2xl py-2 cursor-pointer px-2 w-full hover:bg-itemHover ${selected ? 'font-medium bg-slate-700' : 'font-normal'}`}>
+                            {item.name.charAt(0).toUpperCase() + item.name.slice(1)}
                           </span>
                         </>
                       )}
