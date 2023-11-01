d88cf0a11c322552fb799f10088c7d425d4b1166
davik20 <onyejivic@gmail.com>
2023-11-01 09:13:36 +0100

    update axios

--- apps/events-app/src/axiosInstance.ts
+++ apps/events-app/src/axiosInstance.ts
@@ -2,12 +2,12 @@ import axios, { AxiosInstance } from 'axios';
 import Router from 'next/router';
 
 const axiosInstance: AxiosInstance = axios.create({
-  baseURL: 'https://zuzapp-test-v1-events-app-delta.vercel.app', // replace with your API endpoint
-  // baseURL: 'https://www.zuzalu.city',
-  // baseURL: 'http://localhost:3001',
-  headers: {
-    'Content-Type': 'application/json',
-  },
+    // baseURL: 'https://zuzapp-test-v1-events-app-delta.vercel.app', // replace with your API endpoint
+    // baseURL: 'https://www.zuzalu.city',
+    baseURL: 'http://localhost:3001',
+    headers: {
+        'Content-Type': 'application/json',
+    },
 });
 
 // const axiosInstance: AxiosInstance = axios.create({
@@ -30,31 +30,31 @@ const axiosInstance: AxiosInstance = axios.create({
 
 // Response interceptor
 axiosInstance.interceptors.response.use(
-  (response) => {
-    // Do something with the response data
-    return response;
-  },
-  (error) => {
-    if (error.response) {
-      switch (error.response.status) {
-        case 401:
-          Router.push('/');
-          break;
-        // case 403:
+    (response) => {
+        // Do something with the response data
+        return response;
+    },
+    (error) => {
+        if (error.response) {
+            switch (error.response.status) {
+                case 401:
+                    Router.push('/');
+                    break;
+                // case 403:
 
-        //     alert('Forbidden. You do not have permission.');
-        //     break;
-        // case 500:
+                //     alert('Forbidden. You do not have permission.');
+                //     break;
+                // case 500:
 
-        //     alert('Server error. Please try again later.');
-        //     break;
-        // default:
+                //     alert('Server error. Please try again later.');
+                //     break;
+                // default:
 
-        //     alert('Something went wrong.');
-      }
+                //     alert('Something went wrong.');
+            }
+        }
+        return Promise.reject(error);
     }
-    return Promise.reject(error);
-  }
 );
 
 export default axiosInstance;
