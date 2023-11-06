84e200730f17dda8ec98b2a60b9e44ce5557ab42
dev_waliba <donwaleyb@gmail.com>
2023-11-06 20:24:12 +0100

    banner conditional fixed

--- apps/events-app/components/eventspace/EventBanner.tsx
+++ apps/events-app/components/eventspace/EventBanner.tsx
@@ -1,7 +1,7 @@
-import { useState, useEffect, useRef } from "react";
-import Button from "../ui/buttons/Button";
-import { HiUpload } from "react-icons/hi";
-import { uploadImage } from "@/controllers/image.controller";
+import { useState, useEffect, useRef } from 'react';
+import Button from '../ui/buttons/Button';
+import { HiUpload } from 'react-icons/hi';
+import { uploadImage } from '@/controllers/image.controller';
 import Image from 'next/image';
 
 export const EventBanner = ({ banner, setBanner }: any) => {
@@ -14,7 +14,7 @@ export const EventBanner = ({ banner, setBanner }: any) => {
     e.preventDefault();
     try {
       const result = await uploadImage(e, 'events');
-      setBanner(result)
+      setBanner(result);
       setImageLink(result);
     } catch (error) {
       console.log(error);
@@ -26,7 +26,7 @@ export const EventBanner = ({ banner, setBanner }: any) => {
     e.stopPropagation();
     setDragActive(false);
     if (e.dataTransfer.files && e.dataTransfer.files[0]) {
-      for (let i = 0; i < e.dataTransfer.files["length"]; i++) {
+      for (let i = 0; i < e.dataTransfer.files['length']; i++) {
         setFiles((prevState: any) => [...prevState, e.dataTransfer.files[i]]);
       }
     }
@@ -51,37 +51,24 @@ export const EventBanner = ({ banner, setBanner }: any) => {
   }
 
   function openFileExplorer() {
-    inputRef.current.value = "";
+    inputRef.current.value = '';
     inputRef.current.click();
   }
 
   return (
     <div className="flex flex-col gap-[10px]">
-      <h2 className="text-lg font-semibold leading-[1.2] text-white self-stretch">
-        Event Banner
-      </h2>
+      <h2 className="text-lg font-semibold leading-[1.2] text-white self-stretch">Event Banner</h2>
       <div className="flex gap-3 items-end">
-        <div
-          className="w-[50%] bg-inputField"
-          onDragEnter={handleDragEnter}
-          onDrop={handleDrop}
-          onDragLeave={handleDragLeave}
-          onDragOver={handleDragOver}
-        >
+        <div className="w-[50%] bg-inputField" onDragEnter={handleDragEnter} onDrop={handleDrop} onDragLeave={handleDragLeave} onDragOver={handleDragOver}>
           <input placeholder="fileInput" className="hidden" ref={inputRef} type="file" onChange={handleChange} accept="image/*" />
           <div className="w-full h-[204px]">
-            <Button
-              className="flex flex-col items-center justify-center rounded-[6px] border-dashed text-white border-white border-opacity-10 bg-pagePrimary h-full w-full"
-              onClick={openFileExplorer}
-            >
+            <Button className="flex flex-col items-center justify-center rounded-[6px] border-dashed text-white border-white border-opacity-10 bg-pagePrimary h-full w-full" onClick={openFileExplorer}>
               <HiUpload />
-              <span className="text-[13px] text-center opacity-50 font-bold leading-[1.2] self-stretch">
-                Select Event Header Image
-              </span>
+              <span className="text-[13px] text-center opacity-50 font-bold leading-[1.2] self-stretch">Select Event Header Image</span>
             </Button>
           </div>
         </div>
-        <Image src={banner} alt="" className="border rounded-lg h-[100px] w-[130px]" width={100} height={130} loading='lazy' />
+        {banner && <Image src={banner} alt="" className="border rounded-lg h-[100px] w-[130px]" width={100} height={130} loading="lazy" />}
       </div>
     </div>
   );
